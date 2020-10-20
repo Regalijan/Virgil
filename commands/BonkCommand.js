7557a9f6db1b47d5f3f231ec2e71a47b96f09f1e
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-19 22:22:45 -0400

    Add bonk command

---
+++ commands/BonkCommand.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "bonk",
+    description: "BONK!",
+    guildOnly: true,
+    execute(message, args) {
+        let target = `<@${args[0]}>`
+        if (args[0].match(/(@)/)) {
+            target = message.mentions.members.first()
+        }
+        const embed = new Discord.MessageEmbed()
+        .setTitle("BONK!")
+        .setImage("https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg")
+        .setDescription(`<@${message.author.id}> has bonked ${target}.`)
+        .setColor(3756250)
+        message.channel.send(embed)
+    }
+}
