377e7aaf6c581681a90b2445ae850b9203846965
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-20 12:59:48 -0400

    Add additions to verify command

--- commands/VerifyCommand.js
+++ commands/VerifyCommand.js
@@ -1,4 +1,3 @@
-const Discord = require('discord.js')
 const request = require('axios')
 
 module.exports = {
@@ -8,7 +7,7 @@ module.exports = {
     execute(message) {
         let RobloxID = undefined
         let groupRank = 0
-        const { groupranks } = require(`../serversettings/${message.guild.id}.json`)
+        const { badges, groupranks, gamepasses } = require(`../serversettings/${message.guild.id}.json`)
         request(`https://verify.eryn.io/api/user/${message.author.id}`)
         .then(function (roverResponse) {
             if (roverResponse.status != 200) {
@@ -18,16 +17,38 @@ module.exports = {
                         RobloxID = bloxlinkResponse.data.primaryAccount
                         request(`https://api.roblox.com/users/${RobloxID}/groups`)
                         .then(function (groupInfo) {
-                            for(var i = 0;i < groupInfo.data.length; i++) {
-                                for(var x = 0; x < groupranks.length; x++) {
+                            for(var i = 0;i < groupInfo.data.length;i++) {
+                                for(var x = 0;x < groupranks.length;i++) {
                                     if ((groupranks[x].group.includes(groupInfo.data[i].Id)) && (groupranks[x].rank.includes(groupInfo.data[i].Rank))) {
                                         groupRank = groupInfo.data[i].Rank
-                                        /* message.member.addRole(groupranks[x].role) */
-                                        return message.reply(`Your group rank in Car Crushers is ${groupRank}, with the associated discord role of <@&${groupranks[x].role}>`)
+                                        message.member.addRole(groupranks[x].role)
                                     }
                                 }
                             }
                         })
+                        .catch(e => console.error(e.stack))
+                        if (gamepasses) {
+                            for (let i = 0;i < gamepasses.length;i++) {
+                                request(`https://inventory.roblox.com/v1/users/${RobloxID}/items/GamePass/${gamepasses[i]}`)
+                                .then(gamepassData => {
+                                    if (gamepassData.data.data) {
+                                        if (!message.member.roles.cache.has(gamepasses[i].role)) message.member.roles.add(gamepasses[i].role).catch(e => console.error(e.stack))
+                                    }
+                                })
+                                .catch(e => console.error(e.stack))
+                            }
+                        }
+                        if (badges) {
+                            for (let i = 0;i < badges.length;i++) {
+                                request(`https://inventory.roblox.com/v1/users/${RobloxID}/items/Badge/${badges[i]}`)
+                                .then(badgeData => {
+                                    if (badgeData.data.data) {
+                                        if (!message.member.roles.cache.has(badges[i].role)) message.member.roles.add(badges[i].role).catch(e => console.error(e.stack))
+                                    }
+                                })
+                                .catch(e => console.error(e.stack))
+                            }
+                        }
                     }
                     else {
                         return message.reply('You are not verified on either RoVer or BloxLink, please visit https://verify.eryn.io to link your account to RoVer')
@@ -47,8 +68,7 @@ module.exports = {
                         for(var x = 0; x < groupranks.length; x++) {
                             if ((groupranks[x].group.includes(groupInfo.data[i].Id)) && (groupranks[x].rank.includes(groupInfo.data[i].Rank))) {
                                 groupRank = groupInfo.data[i].Rank
-                                /* message.member.addRole(groupranks[x].role) */
-                                return message.reply(`Your group rank in Car Crushers is ${groupRank}, with the associated discord role of <@&${groupranks[x].role}>`)
+                                message.member.addRole(groupranks[x].role).catch(e => console.error(e.stack))
                             }
                         }
                     }
