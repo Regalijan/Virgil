eadf44ff5308f8726041f23fc9f04185181e7fa0
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-22 13:42:31 -0400

    Slim down verify command

--- commands/VerifyCommand.js
+++ commands/VerifyCommand.js
@@ -5,79 +5,63 @@ module.exports = {
     description: 'Adds roles based on your roblox badges and group rank (if applicable).',
     guildOnly: true,
     execute(message) {
-        let RobloxID = undefined
-        let groupRank = 0
+        let robloxId = undefined
+        let roverData = undefined
+        let bloxlinkData = undefined
         const { badges, groupranks, gamepasses } = require(`../serversettings/${message.guild.id}.json`)
-        request(`https://verify.eryn.io/api/user/${message.author.id}`)
-        .then(function (roverResponse) {
-            if (roverResponse.status != 200) {
-                request(`https://api.blox.link/v1/user/${message.author.id}`)
-                .then(function (bloxlinkResponse) {
-                    if (bloxlinkResponse.data.status === "ok") {
-                        RobloxID = bloxlinkResponse.data.primaryAccount
-                        request(`https://api.roblox.com/users/${RobloxID}/groups`)
-                        .then(function (groupInfo) {
-                            for(var i = 0;i < groupInfo.data.length;i++) {
-                                for(var x = 0;x < groupranks.length;i++) {
-                                    if ((groupranks[x].group.includes(groupInfo.data[i].Id)) && (groupranks[x].rank.includes(groupInfo.data[i].Rank))) {
-                                        groupRank = groupInfo.data[i].Rank
-                                        message.member.addRole(groupranks[x].role)
-                                    }
-                                }
-                            }
-                        })
-                        .catch(e => console.error(e.stack))
-                        if (gamepasses) {
-                            for (let i = 0;i < gamepasses.length;i++) {
-                                request(`https://inventory.roblox.com/v1/users/${RobloxID}/items/GamePass/${gamepasses[i]}`)
-                                .then(gamepassData => {
-                                    if (gamepassData.data.data) {
-                                        if (!message.member.roles.cache.has(gamepasses[i].role)) message.member.roles.add(gamepasses[i].role).catch(e => console.error(e.stack))
-                                    }
-                                })
-                                .catch(e => console.error(e.stack))
-                            }
-                        }
-                        if (badges) {
-                            for (let i = 0;i < badges.length;i++) {
-                                request(`https://inventory.roblox.com/v1/users/${RobloxID}/items/Badge/${badges[i]}`)
-                                .then(badgeData => {
-                                    if (badgeData.data.data) {
-                                        if (!message.member.roles.cache.has(badges[i].role)) message.member.roles.add(badges[i].role).catch(e => console.error(e.stack))
-                                    }
-                                })
-                                .catch(e => console.error(e.stack))
-                            }
-                        }
-                    }
-                    else {
-                        return message.reply('You are not verified on either RoVer or BloxLink, please visit https://verify.eryn.io to link your account to RoVer')
-                    }
-                })
-                .catch(function (error) {
-                    console.error(error)
-                    return message.channel.send(`D0h! Some monkeying happened on my end! ${error}`)
-                })
-                
+        try {
+            roverData = await request(`https://verify.eryn.io/api/user/${message.author.id}`)
+        }
+        catch (e) {
+            console.error(e.stack)
+        }
+        if (roverData.status == 200) {
+            robloxId = roverData.data.robloxId
+        }
+        else {
+            try {
+                bloxlinkData = await request(`https://api.blox.link/v1/user/${message.author.id}`)
+                if (bloxlinkData.data.status === "ok") robloxId = bloxlinkData.data.primaryAccount
             }
-            else {
-                RobloxID = roverResponse.data.robloxId
-                request(`https://api.roblox.com/users/${RobloxID}/groups`)
-                .then(function (groupInfo) {
-                    for(var i = 0;i < groupInfo.data.length; i++) {
-                        for(var x = 0; x < groupranks.length; x++) {
-                            if ((groupranks[x].group.includes(groupInfo.data[i].Id)) && (groupranks[x].rank.includes(groupInfo.data[i].Rank))) {
-                                groupRank = groupInfo.data[i].Rank
-                                message.member.addRole(groupranks[x].role).catch(e => console.error(e.stack))
-                            }
+            catch (e) {
+                console.error(e.stack)
+            }
+        }
+        if (!roverData.data.robloxId && !bloxlinkData.data.primaryAccount) return message.channel.send('You are not verified in either of my databases!')
+        try {
+            const groupData = await request(`https://groups.roblox.com/v1/users/${robloxId}/groups/roles`)
+            if (groupData.status == 400) return message.channel.send('I could not check group ranks as your user account appears to be deleted!')
+            else if ((groupData.status != 200) && (groupData.status != 400)) return message.channel.send('I could not retrieve group ranks as Roblox is currently having problems!')
+            if (groupData.data.data) {
+                for (let i = 0;i < groupData.data.data.length;i++) {
+                    for (let x = 0;x < groupranks.length;x++) {
+                        if ((groupranks[x].rank.includes(groupData.data.data[i].role.rank)) && (message.member.guild.me.hasPermission("MANAGE_ROLES"))) {
+                            message.member.roles.add(groupranks[x].role).catch(e => console.error(e.stack))
                         }
                     }
-                })
-                .catch(function (error) {
-                    console.error(error)
-                    return message.channel.send(`D0h! Some monkeying happened on my end! ${error}`)
-                })
+                }
             }
-        })
+            if (badges) {
+                for (let i = 0;i < badges.length;i++) {
+                    let badgeData = await request(`https://inventory.roblox.com/v1/users/${robloxId}/items/Badge/${badges[i].badge}`)
+                    if ((badgeData.data.data) && (message.member.guild.me.hasPermission("MANAGE_ROLES")) && (member)) {
+                        message.member.roles.add(badges[i].role).catch(e => console.error(e.stack))
+                    }
+                }
+            }
+            if (gamepasses) {
+                for (let i = 0;i < gamepasses.length;i++) {
+                    let gamepassData = await request(`https://inventory.roblox.com/v1/users/${robloxId}/items/GamePass/${gamepasses[i].gamepass}`)
+                    if (gamepassData.data.data) {
+                        if ((gamepassData.data.data[0].id == gamepasses[i].gamepass) && (message.member.guild.me.hasPermission("MANAGE_ROLES")) && (member)) {
+                            message.member.roles.add(gamepasses[i].role).catch(e => console.error(e => console.error(e.stack)))
+                        }
+                    }
+                }
+            }
+        }
+        catch (e) {
+            console.error(e.stack)
+        }
     }
 }
