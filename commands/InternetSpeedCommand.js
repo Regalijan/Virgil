2687d47898dd2642fe730b642a2a42e7d080faa0
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-29 00:09:27 -0400

    Create internetspeed command

---
+++ commands/InternetSpeedCommand.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'internetspeed',
+    description: 'What do you think this is lol',
+    execute(message) {
+        const embed = new Discord.MessageEmbed()
+        .setImage('https://thumbsnap.com/sc/3N5uU9CP.png')
+        .setColor(3756250)
+        return message.channel.send(embed)
+    }
+}
