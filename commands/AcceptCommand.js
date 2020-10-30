c02a4644a56da68243374fa69231174aadcd11e9
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-10-30 12:00:13 -0400

    Accept command was using wrong data for api key

--- commands/AcceptCommand.js
+++ commands/AcceptCommand.js
@@ -29,7 +29,7 @@ module.exports = {
                             method: 'POST',
                             url: sendingUrl,
                             headers: {
-                                'Authorization': `Basic ${config.mailgunApiKey}`
+                                'Authorization': `Basic ${apiKey}`
                             },
                             form: form
                         })
