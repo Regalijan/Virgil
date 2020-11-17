960f8bc697374415d20032bbf64dd80d02ea59b4
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-17 11:30:10 -0500

    Fix wacky total ping number

--- commands/PingCommand.js
+++ commands/PingCommand.js
@@ -1,16 +1,10 @@
 const client = require('../index')
-const Discord = require('discord.js')
 
 module.exports = {
     name: "ping",
-    description: "pong",
-    execute(message) {
-        console.log()
-        const embed = new Discord.MessageEmbed()
-        .setTitle('PONG!')
-        .setColor(3756250)
-        .addField('Total', `${Date.now() - message.createdTimestamp}ms`, true)
-        .addField('WebSocket',`${client.client.ws.ping}ms`,true)
-        return message.channel.send(embed)
+    description: "Pong",
+    async execute(message) {
+        const ping = await message.channel.send(`:bulb: Checking...`)
+        ping.edit(`:ping_pong: WebSocket: ${client.client.ws.ping}ms, Total: ${ping.createdTimestamp - message.createdTimestamp}ms`)
     }
 }
