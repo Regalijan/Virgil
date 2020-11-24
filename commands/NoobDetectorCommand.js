131285bccb829be1f98982ace1020c5eea3a5d9d
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-24 14:30:38 -0500

    Add support for noobing others

--- commands/NoobDetectorCommand.js
+++ commands/NoobDetectorCommand.js
@@ -4,9 +4,15 @@ module.exports = {
     name: 'noobdetector',
     description: 'Find out how much of a noob you are',
     execute(message) {
+        let member = message.member
+        if (args[0]) member = args[0]
+        if (args[0] && args[0].match(/(^<@!?[0-9]*>)/)) {
+            member = message.mentions.members.first()
+        }
+        else if (args[0] && message.guild.member(args[0])) member = message.guild.member(member)
         const embed = new Discord.MessageEmbed()
         .setTitle('Noob Detector')
-        .setAuthor(message.author.tag,message.author.displayAvatarURL())
+        .setAuthor(member.user.tag,member.user.displayAvatarURL())
         .setColor(3756250)
         .addField('Noob Percentage',Math.floor(Math.random()*101).toString())
         message.channel.send(embed)
