2340a0004687d17f53256389c734744c89ef36c0
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-24 23:16:00 -0500

    Properly fetch users who are not cached

--- commands/NoobDetectorCommand.js
+++ commands/NoobDetectorCommand.js
@@ -3,18 +3,15 @@ const Discord = require('discord.js')
 module.exports = {
     name: 'noobdetector',
     description: 'Find out how much of a noob you are',
-    execute(message) {
+    async execute(message, args) {
         let member = message.member
-        if (args[0]) member = args[0]
-        if (args[0] && args[0].match(/(^<@!?[0-9]*>)/)) {
-            member = message.mentions.members.first()
-        }
-        else if (args[0] && message.guild.member(args[0])) member = message.guild.member(member)
+        if (args[0] && args[0].match(/(^<@!?[0-9]*>)/)) member = message.mentions.members.first()
+        else if (args[0]) member = await message.guild.members.fetch(args[0]).catch(e => console.error(e))
         const embed = new Discord.MessageEmbed()
         .setTitle('Noob Detector')
         .setAuthor(member.user.tag,member.user.displayAvatarURL())
         .setColor(3756250)
-        .addField('Noob Percentage',Math.floor(Math.random()*101).toString())
+        .addField('Noob Percentage',`${Math.floor(Math.random()*101)}%`)
         message.channel.send(embed)
     }
 }
