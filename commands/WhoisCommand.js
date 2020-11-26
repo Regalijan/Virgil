9672c72475400cf009c89290db6b9aac5f8c5393
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-25 23:53:51 -0500

    Create standard whois command

--- commands/WhoisCommand.js
+++ commands/WhoisCommand.js
@@ -1,76 +1,25 @@
-const request = require('axios')
-const Discord = require('discord.js')
-const config = require('../config.json')
+const Discord = require("discord.js")
 
 module.exports = {
     name: "whois",
-    description: "Looks up a user's roblox information",
+    description: "User lookup",
     guildOnly: true,
-    async execute(message, args) {
-        let user = message.author.id
-        let robloxId = 'Unknown'
-        let roverData = undefined
-        let bloxlinkData = undefined
-        let robloxData = undefined
-        let profile = undefined
-        let profileSource = undefined
-        let joinDate = 'Unknown'
-        let pastNames = 'None'
-        let bio = 'Unknown'
-        let avatar = 'Unknown'
+    async execute(message,args) {
+        let member = message.member
+        if (args[0] && args[0].match(/(^<@!?[0-9]*>)/)) member = message.mentions.members.first()
+        else if (args[0]) member = await message.guild.members.fetch(args[0]).catch(e => console.error(e))
         const embed = new Discord.MessageEmbed()
-        if (args[0]) user = args[0]
-        else if (user.match(/(^<@!?[0-9]*>)/)) {
-            user = message.mentions.members.first().id
-        }
-        roverData = await request(`https://verify.eryn.io/api/user/${user}`, {validateStatus: false}).catch(e => {
-            console.log('Failed to fetch data from RoVer!')
-            console.error(e.stack)
-        })
-        if (roverData.status == 200) {
-            robloxId = roverData.data.robloxId
-        }
-        else {
-            bloxlinkData = await request(`https://api.blox.link/v1/user/${user}`, {validateStatus: false}).catch(e => {
-                console.log('Failed to fetch data from BloxLink!')
-                console.error(e)
-                return message.channel.send('I could not retreive this user\'s data!')
-            })
-            if (bloxlinkData.data.status === "ok") robloxId = bloxlinkData.data.primaryAccount
-        }
-        if (!roverData.data.robloxId && !bloxlinkData.data.primaryAccount) return message.channel.send('This user could not be found!')
-        robloxData = await request(`https://users.roblox.com/v1/users/${robloxId}`).catch(e => {
-            console.error(e)
-            return message.channel.send('Hmm........ something broke. Roblox might be giving me garbage instead of data.')
-        })
-        if (robloxData.data.isBanned) return message.channel.send('Looks like Roblox deleted this account.')
-        avatar = `https://assetgame.roblox.com/Thumbs/Avatar.ashx?username=${robloxData.data.name}`
-        profile = `https://www.roblox.com/users/${robloxData.data.id}/profile`
-        profileSource = await request(profile).catch(e => console.error(e))
-        joinDate = profileSource.data.match(/Join Date<p class=text-lead>(.*?)<li/)[1]
-        bio = profileSource.data.match(/<meta name=description content=".*? is one of the millions playing, creating and exploring the endless possibilities of Roblox. Join .*? on Roblox and explore together! ?((?:.|\n)*?)"/m)[1]
-        if(profileSource.data.match(/<span class=tooltip-pastnames data-toggle=tooltip title="?(.*?)"?>/)) {
-            pastNames = profileSource.data.match(/<span class=tooltip-pastnames data-toggle=tooltip title="?(.*?)"?>/)[1].substr(0, 1024)
-        }
-        while ((bio.match(/\n/mg) || []).length > 3) {
-            const lastN = bio.lastIndexOf('\n')
-            bio = bio.slice(0, lastN) + bio.slice(lastN + 1)
-        }
-        if (bio.length > 500) {
-            bio = bio.substr(0, 500) + '...'
-        }
-        bio = bio.replace('@', '@ ')
-        embed.setTitle('View Profile')
-        .setURL(profile)
-        .setAuthor(robloxData.data.name,avatar,profile)
+        .setTitle('User Info')
+        .setDescription(`Profile of ${member}`)
         .setColor(3756250)
-        .setThumbnail(avatar)
-        .setDescription(bio)
+        .setThumbnail(member.user.displayAvatarURL())
         .addFields(
-            {name: 'Join Date', value: joinDate,inline: true},
-            {name: 'Past Usernames',value: pastNames,inline: true}
+            {name: 'Username', value: member.user.tag, inline: true},
+            {name: 'User ID', value: member.id, inline: true},
+            {name: 'Highest Role', value: member.roles.highest, inline: true},
+            {name: 'Created At', value: member.user.createdAt, inline: true},
+            {name: 'Joined At', value: member.joinedAt, inline: true}
         )
-        if (config.owner == user) embed.addField('User Tags','Bot Creator',true)
-        return message.channel.send(embed)
+        message.channel.send(embed)
     }
 }
