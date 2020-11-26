5cfc6339180abf45fe7e6d49e201ca181e405098
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-26 02:36:56 -0500

    Duck command now uses async/await

--- commands/DuckCommand.js
+++ commands/DuckCommand.js
@@ -4,19 +4,19 @@ const request = require('axios')
 module.exports = {
     name: "duck",
     description: "Shows a random duck.",
-    execute(message) {
-        request('https://random-d.uk/api/v2/random')
-        .then(function (response) {
+    async execute(message) {
+        try {
+            const response = await request('https://random-d.uk/api/v2/random')
             const embed = new Discord.MessageEmbed()
             .setTitle(':duck: QUACK! A random duck for you!')
-            .setImage(response.data.url)
             .setColor(3756250)
+            .setImage(response.data.url)
             .setFooter(response.data.message)
             return message.channel.send(embed)
-        })
-        .catch(function (error) {
-            console.error(error)
-            return message.channel.send(`D0h! Some monkeying happened on my end! ${error}`)
-        })
+        }
+        catch(e) {
+            console.error(e)
+            return message.channel.send('Something broke on my end! If this keeps happening, contact the bot developer.')
+        }
     }
 }
