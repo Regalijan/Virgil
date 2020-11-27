8b2e039fcc73a2c03ca48aef98071246e051f311
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-27 16:33:42 -0500

    Add support for searching by name

--- commands/NoobDetectorCommand.js
+++ commands/NoobDetectorCommand.js
@@ -4,9 +4,11 @@ module.exports = {
     name: 'noobdetector',
     description: 'Find out how much of a noob you are',
     async execute(message, args) {
+        let validmember = true
         let member = message.member
         if (args[0] && args[0].match(/(^<@!?[0-9]*>)/)) member = message.mentions.members.first()
-        else if (args[0]) member = await message.guild.members.fetch(args[0]).catch(e => console.error(e))
+        else if (args[0]) member = await message.guild.members.fetch(args[0]).catch(e => {if (e.httpStatus == 400) return validmember = false})
+        if (!validmember) await message.guild.members.fetch({query: args[0], limit: 1}).then(results => {results.mapValues(values => {return member = values})})
         const embed = new Discord.MessageEmbed()
         .setTitle('Noob Detector')
         .setAuthor(member.user.tag,member.user.displayAvatarURL())
