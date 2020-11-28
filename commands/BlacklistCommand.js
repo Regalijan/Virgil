382d8b20ab0d88a97bbce9a231fdbea4d1c6c6b6
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-28 02:28:24 -0500

    Use async/await for blacklist command

--- commands/BlacklistCommand.js
+++ commands/BlacklistCommand.js
@@ -1,48 +1,37 @@
 const fs = require('fs')
 const request = require('axios')
-const { banFilesPath, bucket, gameModeratorRole, gameModeratorUsers, serviceKeyPath } = require('../config.json')
+const { bucket, gameModeratorRole, gameModeratorUsers, serviceKeyPath } = require('../config.json')
 const {Storage} = require('@google-cloud/storage')
 const storage = new Storage({keyFilename: serviceKeyPath})
 module.exports = {
     name: 'blacklist',
     description: 'Blacklists user from game',
     guildOnly: true,
-    execute(message, args) {
-        let errormessage = undefined
+    async execute(message, args) {
         if (gameModeratorUsers.includes(message.author.id) || message.member.roles.cache.some(role => gameModeratorRole.includes(role.id))) {
             if (args[0]) {
-                request(`https://api.roblox.com/users/get-by-username?username=${args[0]}`)
-                .then(function (response) {
-                    if ((!response.data.Id) || (response.data.Id == '')) {
-                        return message.reply(`I could not find a Roblox user with the name of ${args[0]}`)
-                    }
-                    else {
-                        fs.writeFileSync(`${banFilesPath}\\${response.data.Id}.json`, `{"usercode":"0x1"}`, function (err) {
-                            if (err) return errormessage = err
-                        })
-                        async function uploadFile() {
-                            await storage.bucket(bucket).upload(`${banFilesPath}\\${response.data.Id}.json`)
-                        }
-                        if (!errormessage) {
-                            uploadFile()
-                            uploadFile().catch(e => {
-                                console.log(e)
-                                return errormessage = `Error returned by Google!: ${e}`
-                            })
-                            if(errormessage) return errormessage
-                        }
-                        if (!errormessage) {
-                            return message.reply(`${response.data.Username} was blacklisted!`)
-                        }
-                        else {
-                            return message.channel.send(errormessage)
-                        }
+                const robloxData = await request(`https://api.roblox.com/users/get-by-username?username=${args[0]}`).catch(e => {
+                    console.error(e)
+                    return message.channel.send(`An error occured when looking up this user! ${e}`)
+                })
+                if (!robloxData.data.Id || robloxData.data.Id == '') return message.channel.send(`I could not find a Roblox user with the name of ${args[0]}`)
+                fs.writeFile(`./${robloxData.data.Id}.json`, '{"usercode":"0x1"}', err => {
+                    if (err) {
+                        console.error(err)
+                        return message.channel.send(`An error occured when writing the file! ${e}`)
                     }
                 })
-                .catch(function (error) {
-                    console.error(error)
-                    return message.channel.send('An unexpected error occured when finding the user.')
+                await storage.bucket(bucket).upload(`./${robloxData.data.Id}.json`).catch(e => {
+                    console.error(e)
+                    return message.channel.send(`Error returned by Google: ${e}`)
                 })
+                const viewcheck = await request(`https://storage.googleapis.com/${bucket}/${robloxData.data.Id}`, {validateStatus: false})
+                if (viewcheck.status == 403) await storage.bucket(bucket).file(`${robloxData.data.id}.json`).makePublic().catch(e => {
+                    console.error(e)
+                    return message.channel.send(`An error occured when making the file public! ${e}`)
+                })
+                message.channel.send(`${robloxData.data.Username} successfully blacklisted!`)
+                fs.unlink(`./${robloxData.data.Id}.json`, err => console.error(err))
             }
             else {
                 return message.reply('You did not provide a username!')
