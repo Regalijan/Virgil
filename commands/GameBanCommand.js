82de0e783b3a427ad65808ff5e0e8065d0bf36b7
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-28 02:28:36 -0500

    Use async/await for gameban command

--- commands/GameBanCommand.js
+++ commands/GameBanCommand.js
@@ -1,40 +1,41 @@
 const fs = require('fs')
 const request = require('axios')
-const { banFilesPath, bucket, gameModeratorRole, gameModeratorUsers, serviceKeyPath } = require('../config.json')
+const { bucket, gameModeratorRole, gameModeratorUsers, serviceKeyPath } = require('../config.json')
 const {Storage} = require('@google-cloud/storage')
 const storage = new Storage({keyFilename: serviceKeyPath})
 module.exports = {
     name: 'gameban',
     description: 'Bans user from game',
     guildOnly: true,
-    execute(message, args) {
+    async execute(message, args) {
         if (gameModeratorUsers.includes(message.author.id) || message.members.roles.cache.some(role => gameModeratorRole.includes(role.id))) {
             let reason = args.slice(1).join(" ")
             if ((args[0]) && (reason)) {
-                request(`https://api.roblox.com/users/get-by-username?username=${args[0]}`)
-                .then(function (response) {
-                    if ((!response.data.Id) || (response.data.Id == '')) {
-                        return message.reply(`I could not find a Roblox user with the name of ${args[0]}`)
-                    }
-                    else {
-                        fs.writeFileSync(`${banFilesPath}\\${response.data.Id}.json`, `{"usercode":"0x2","reason":"${reason}"}`, function (err) {
-                            if (err) return message.reply(err)
-                        })
-                        async function uploadFile() {
-                            await storage.bucket(bucket).upload(`${banFilesPath}\\${response.data.Id}.json`)
-                        }
-                        uploadFile()
-                        uploadFile().catch(e => {
-                            console.log(e)
-                            return message.channel.send(`Error returned by Google!: ${e}`)
-                        })
-                        return message.reply(`${response.data.Username} was banned from the game!`)
+                const robloxData = await request(`https://api.roblox.com/users/get-by-username?username=${args[0]}`).catch(e => {
+                    console.error(e)
+                    return message.channel.send(`An error occured when looking up this user! ${e}`)
+                })
+                if (!robloxData.data.Id || robloxData.data.Id == '') return message.channel.send(`I could not find a Roblox user with the name of ${args[0]}.`)
+                fs.writeFile(`./${robloxData.data.Id}.json`, `{"usercode":"0x2","reason":"${reason}"}`, err => {
+                    if (err) {
+                        console.error(err)
+                        return message.channel.send(`An error occured when writing the file!`)
                     }
                 })
-                .catch(function (error) {
-                    console.error(error)
-                    return message.channel.send('An unexpected error occured when finding the user.')
+                await storage.bucket(bucket).upload(`./${robloxData.data.Id}.json`).catch(e => {
+                    console.error(e)
+                    return message.channel.send(`An error occured when uploading the file! ${e}`)
                 })
+                const filecheck = await request(`https://storage.googleapis.com/${bucket}/${robloxData.data.Id}.json`, {validateStatus: false}).catch(e => {
+                    console.error(e)
+                    return message.channel.send(`An error occured when checking the file! ${e}`)
+                })
+                if (filecheck.status == 403) await storage.bucket(bucket).file(`${robloxData.data.Id}.json`).makePublic().catch(e => {
+                    console.error(e)
+                    return message.channel.send(`An error occured when making the file public! ${e}`)
+                })
+                message.channel.send(`${robloxData.data.Username} successfully banned from the game!`)
+                fs.unlink(`./${robloxData.data.Id}.json`, e => console.error(e))
             }
             else if (!args[0]) {
                 return message.reply('You did not provide a username!')
