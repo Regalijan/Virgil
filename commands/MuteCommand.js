e25cfb4c14dd512c3fcbe6a4176208395c802647
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-30 13:05:50 -0500

    More standard formatting

--- commands/MuteCommand.js
+++ commands/MuteCommand.js
@@ -13,14 +13,14 @@ module.exports = {
           member = message.mentions.members.first()
         } else if (message.guild.member(member)) member = message.guild.member(member)
         else return message.channel.send('I could not find this user.')
-        if (message.member == member) return message.channel.send('Dumbass, why would you want to mute yourself?')
+        if (message.member === member) return message.channel.send('Dumbass, why would you want to mute yourself?')
         if (member.roles.cache) {
           if (member.roles.cache.find(role => serversettings.moderatorRoles.includes(role.id))) return message.channel.send('I cannot mute moderators.')
         }
         if (!reason) reason = 'No reason provided.'
         if (!serversettings.muteRole) return message.channel.send('Muted role was not set!')
-        if (!guild.roles.cache.find(role => role.id === serversettings.muteRole)) return message.channel.send('The muted role is invalid!')
-        const role = guild.roles.cache.find(role => role.id === serversettings.muteRole)
+        if (!message.guild.roles.cache.find(role => role.id === serversettings.muteRole)) return message.channel.send('The muted role is invalid!')
+        const role = message.guild.roles.cache.find(role => role.id === serversettings.muteRole)
         member.roles.add(role)
         message.channel.send(`${member} was muted!`)
       }
