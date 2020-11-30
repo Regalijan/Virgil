e365b4bec28e319ce7f6e538fdbeed976b4e9300
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-11-30 12:56:40 -0500

    Regex incorrectly stringified

--- commands/ReportCommand.js
+++ commands/ReportCommand.js
@@ -10,7 +10,7 @@ module.exports = {
       const reason = args.slice(2).join(' ')
       request(`https://api.roblox.com/users/get-by-username?username=${args[0]}`)
         .then(response => {
-          if ((response.status == 200) && (args[1].match('^(https:\/\/|http:\/\/|<https:\/\/|<http:\/\/.)'))) {
+          if ((response.status === 200) && (args[1].match(/^(https:\/\/|http:\/\/|<https:\/\/|<http:\/\/.)/))) {
             const embed = new Discord.MessageEmbed()
               .setTitle('Exploiter Report')
               .setDescription(`${message.author.username}#${message.author.discriminator} has reported ${response.data.Username} for exploiting!\n\nReason: ${args[2]}\n\n[Evidence](${reason})`)
@@ -20,7 +20,7 @@ module.exports = {
             }
           } else if (response.status == 404) {
             message.channel.send('I could not find this user, is the spelling correct? (Usernames are not case-sensitive)')
-          } else if (!args[1].match('^(https:\/\/|http:\/\/|<https:\/\/|<http:\/\/.)')) {
+          } else if (!args[1].match(/^(https:\/\/|http:\/\/|<https:\/\/|<http:\/\/.)/)) {
             message.channel.send('The URL you sent was not valid, make sure it starts with `http://` or `https://`')
           } else {
             message.channel.send('An unknown error occured! Maybe Roblox is down or is returning malformed data. If this keeps happening, contact the bot developer.')
