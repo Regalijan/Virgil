62e3b2ac25e652222f455ccc1a40f8c1ca679f99
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-01 15:11:09 -0500

    Use users api instead of parsing profile page

--- commands/RbxInfoCommand.js
+++ commands/RbxInfoCommand.js
@@ -9,20 +9,15 @@ module.exports = {
   async execute (message, args) {
     let user = message.author.id
     let robloxId = 'Unknown'
-    let bloxlinkData
-    let joinDate = 'Unknown'
-    let pastNames = 'None'
-    let bio = 'Unknown'
-    let avatar = 'Unknown'
-    const embed = new Discord.MessageEmbed()
     if (args[0]) user = args[0]
-    else if (user.match(/(^<@!?[0-9]*>)/)) {
+    if (user.match(/(^<@!?[0-9]*>)/)) {
       user = message.mentions.members.first().id
     }
     const roverData = await request(`https://verify.eryn.io/api/user/${user}`, { validateStatus: false }).catch(e => {
       console.log('Failed to fetch data from RoVer!')
       console.error(e.stack)
     })
+    let bloxlinkData
     if (roverData.status === 200) {
       robloxId = roverData.data.robloxId
     } else {
@@ -38,15 +33,10 @@ module.exports = {
       console.error(e)
       return message.channel.send('Hmm........ something broke. Roblox might be giving me garbage instead of data.')
     })
-    if (robloxData.data.isBanned) return message.channel.send('Looks like Roblox deleted this account.')
-    avatar = `https://assetgame.roblox.com/Thumbs/Avatar.ashx?username=${robloxData.data.name}`
-    const profile = `https://www.roblox.com/users/${robloxData.data.id}/profile`
-    const profileSource = await request(profile).catch(e => console.error(e))
-    joinDate = profileSource.data.match(/Join Date<p class=text-lead>(.*?)<li/)[1]
-    bio = profileSource.data.match(/<meta name=description content=".*? is one of the millions playing, creating and exploring the endless possibilities of Roblox. Join .*? on Roblox and explore together! ?((?:.|\n)*?)"/m)[1]
-    if (profileSource.data.match(/<span class=tooltip-pastnames data-toggle=tooltip title="?(.*?)"?>/)) {
-      pastNames = profileSource.data.match(/<span class=tooltip-pastnames data-toggle=tooltip title="?(.*?)"?>/)[1].substr(0, 1024)
-    }
+    let bio = robloxData.data.description
+    const joinDate = new Date(robloxData.data.created)
+    if (robloxData.data.isBanned) return message.channel.send('This account has been terminated by Roblox!')
+    const avatar = `https://assetgame.roblox.com/Thumbs/Avatar.ashx?username=${robloxData.data.name}`
     while ((bio.match(/\n/mg) || []).length > 3) {
       const lastN = bio.lastIndexOf('\n')
       bio = bio.slice(0, lastN) + bio.slice(lastN + 1)
@@ -55,14 +45,26 @@ module.exports = {
       bio = bio.substr(0, 500) + '...'
     }
     bio = bio.replace('@', '@ ')
-    embed.setTitle('View Profile')
+    const pastNamesData = await request(`https://users.roblox.com/v1/users/${robloxId}/username-history?limit=50`).catch(e => {
+      console.error(e)
+      pastNames = 'Unknown'
+    })
+    let pastNames = 'None'
+    if (pastNamesData.data.data[0]) pastNames = pastNamesData.data.data[0].name
+    for (let i = 1; i < pastNamesData.data.data.length; i++) {
+      pastNames = `${pastNames}, ${pastNamesData.data.data[i].name}`
+    }
+    if (!pastNames) pastNames = 'None'
+    const profile = `https://www.roblox.com/users/${robloxData.data.id}/profile`
+    const embed = new Discord.MessageEmbed()
+      .setTitle('View Profile')
       .setURL(profile)
       .setAuthor(robloxData.data.name, avatar, profile)
       .setColor(3756250)
       .setThumbnail(avatar)
       .setDescription(bio)
       .addFields(
-        { name: 'Join Date', value: joinDate, inline: true },
+        { name: 'Join Date', value: `${joinDate.getMonth() + 1}/${joinDate.getDate()}/${joinDate.getFullYear()}`, inline: true },
         { name: 'Past Usernames', value: pastNames, inline: true }
       )
     if (config.owner === user) embed.addField('User Tags', 'Bot Creator', true)
