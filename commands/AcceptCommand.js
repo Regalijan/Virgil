ea41a4807ff42fa9a11b8d90e105af684e82fcb1
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-06 03:28:40 -0500

    No TrAiLiNg SpAcEs aNd CoMmAs MuSt HaVe SpAcEs

--- commands/AcceptCommand.js
+++ commands/AcceptCommand.js
@@ -9,12 +9,12 @@ module.exports = {
   async execute (message, args) {
     if (message.member.roles.cache.some(role => config.appealsManagerRole.includes(role.id))) {
       const userval = args[0]
-      const appeal = await db.query('SELECT * FROM appeals WHERE discord_id = $1;', [userval]).catch(e => { 
+      const appeal = await db.query('SELECT * FROM appeals WHERE discord_id = $1;', [userval]).catch(e => {
         console.error(e)
         return message.channel.send('There was an error looking up this user!')
       })
       if (appeal.rowCount === 0) return message.channel.send('I could not find this user in the database.')
-      await mailer.execute('Appeal Accepted',`<html>Your appeal was accepted, you may join us again at our <a href="${config.appealsInvite}" target="_blank">discord server</a>.<br/><br/>Note from the moderation team: ${args.note}</html>`,appeal.rows[0].email).catch(e => {
+      await mailer.execute('Appeal Accepted', `<html>Your appeal was accepted, you may join us again at our <a href="${config.appealsInvite}" target="_blank">discord server</a>.<br/><br/>Note from the moderation team: ${args.note}</html>`, appeal.rows[0].email).catch(e => {
         console.error(e)
         return message.channel.send('The email could not be sent! Check the console for details.')
       })
