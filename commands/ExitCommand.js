fad15322078702ea02765e35cbae355580a7ff99
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-06 01:44:32 -0500

    Destroy session on exit

--- commands/ExitCommand.js
+++ commands/ExitCommand.js
@@ -1,10 +1,12 @@
 const { owner } = require('../config.json')
+const { client } = require('../index')
 module.exports = {
   name: 'exit',
   description: 'Exits the bot process',
   async execute (message) {
     if (message.author.id === owner) {
       await message.channel.send('Bye.')
+      client.destroy()
       process.exit()
     } else {
       return message.channel.send('You tried.')
