f16db3a92f7c8c3645f4ef897c3769eb4706341c
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-06 03:25:49 -0500

    Make accept command use mailer for sending

--- commands/AcceptCommand.js
+++ commands/AcceptCommand.js
@@ -1,53 +1,28 @@
 const config = require('../config.json')
 const db = require('../database')
-const FormData = require('form-data')
-const request = require('axios')
+const mailer = require('../mailer')
 
 module.exports = {
   name: 'accept',
-  description: 'Accepts user\'s appeal',
+  description: 'Accepts a user\'s appeal',
   guildOnly: true,
-  execute (message, args) {
+  async execute (message, args) {
     if (message.member.roles.cache.some(role => config.appealsManagerRole.includes(role.id))) {
       const userval = args[0]
-      db.query('SELECT * FROM appeals WHERE discord_id = $1;', userval)
-        .then(appeal => {
-          if (appeal.rows[0]) {
-            db.query('SELECT * FROM auth WHERE discord_id = $1;', userval)
-              .then(email => {
-                const form = new FormData()
-                form.append('from', config.fromAddress)
-                form.append('to', email.rows[0].email)
-                form.append('subject', 'Appeal Accepted')
-                form.append('html', `<html>Your appeal was accepted, you may join us again at our <a href="${config.appealsInvite}" target="_blank">discord server</a>.<br/><br/>Note from the moderation team: ${args[1]}</html>`)
-                const apiKey = Buffer.from(`api:${config.mailgunApiKey}`, 'utf8').toString('base64')
-                let sendingUrl = `https://api.mailgun.net/v3/${config.mailgunDomain}/messages`
-                if (config.mailgunRegion === 'eu') {
-                  sendingUrl = `https://api.eu.mailgun.net/v3/${config.mailgunDomain}/messages`
-                }
-                request({
-                  method: 'POST',
-                  url: sendingUrl,
-                  headers: {
-                    Authorization: `Basic ${apiKey}`
-                  },
-                  form: form
-                })
-                  .then(response => {
-                    if (response.status === 200) {
-                      db.query('DELETE FROM appeals WHERE discord_id = $1;', userval)
-                        .catch(e => console.error(e))
-                      return message.channel.send('Appeal accepted!')
-                    }
-                    return message.channel.send(`Mailgun returned an error! ${response.status.code} ${response.body}`)
-                  })
-                  .catch(e => console.error(e))
-              })
-              .catch(e => console.error(e))
-          } else {
-            return message.channel.send('This user don\'t exist in le database')
-          }
-        })
+      const appeal = await db.query('SELECT * FROM appeals WHERE discord_id = $1;', [userval]).catch(e => { 
+        console.error(e)
+        return message.channel.send('There was an error looking up this user!')
+      })
+      if (appeal.rowCount === 0) return message.channel.send('I could not find this user in the database.')
+      await mailer.execute('Appeal Accepted',`<html>Your appeal was accepted, you may join us again at our <a href="${config.appealsInvite}" target="_blank">discord server</a>.<br/><br/>Note from the moderation team: ${args.note}</html>`,appeal.rows[0].email).catch(e => {
+        console.error(e)
+        return message.channel.send('The email could not be sent! Check the console for details.')
+      })
+      await db.query('DELETE FROM appeals WHERE discord_id = $1;', [userval]).catch(e => {
+        console.error(e)
+        return message.channel.send('I could not finish closing this appeal! The user was most likely emailed but the appeal may still appear in the database.')
+      })
+      message.channel.send('Appeal closed and user emailed!')
     }
   }
 }
