53fc1d9193674f0b6e19ff759d82acde783416b1
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-20 03:21:44 -0500

    Mailer finally works

--- mailer.js
+++ mailer.js
@@ -4,26 +4,17 @@ const request = require('axios')
 module.exports = {
   async execute (subject, body, recipient) {
     if (!config.provider || config.provider.toLowerCase() === 'mailgun') {
-      const email = new FormData()
-        .append('from', config.fromAddress)
-        .append('to', recipient)
-        .append('subject', subject)
-        .append('html', body)
+      const form = new FormData()
+      form.append('from', 'noreply@ccdiscussion.com')
+      form.append('to', recipient)
+      form.append('subject', subject)
+      form.append('html', body)
       if (!config.domain || !config.apiKey) return
       let url
       if (config.mailgunRegion === 'eu') url = `https://api.eu.mailgun.net/v3/${config.domain}/messages`
       else url = `https://api.mailgun.net/v3/${config.domain}/messages`
-      const response = await request({
-        method: 'POST',
-        url: url,
-        headers: {
-          Authorization: `Basic ${Buffer.from(`api:${config.apiKey}`).toString('base64')}`,
-          'Content-Type': 'multipart/form-data'
-        },
-        form: email,
-        validateStatus: false
-      }).catch(e => { return console.error(e) })
-      if (response.status !== 200) throw new Error(`HTTP ${response.status}: ${response.body}`)
+      const response = await request.post(url, form, { headers: form.getHeaders(), auth: { username: 'api', password: config.apiKey, validateStatus: false }}).catch(e => { return console.error(e) })
+      if (response.status !== 200) throw new Error(`HTTP ${response.status}: ${response.data}`)
     } else if (config.provider.toLowerCase() === 'sendgrid') {
       const email = JSON.stringify({
         personalizations: [{ to: [{ email: recipient }] }], from: { email: config.fromAddress }, content: [{ type: 'text/html', value: body }]
@@ -32,8 +23,7 @@ module.exports = {
         method: 'POST',
         url: 'https://api.sendgrid.com/v3/mail/send',
         headers: {
-          Authorization: `Bearer ${config.apiKey}`,
-          'Content-Type': 'application/json'
+          Authorization: `Bearer ${config.apiKey}`
         },
         data: email,
         validateStatus: false
