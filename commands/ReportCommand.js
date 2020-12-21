78a681cb70432857daa36a3fddd04718ccaaf804
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-21 11:11:45 -0500

    Autoreplace youtube studio links

--- commands/ReportCommand.js
+++ commands/ReportCommand.js
@@ -8,13 +8,16 @@ module.exports = {
   async execute (message, args) {
     const { exploiterReportChannel } = require(`../serversettings/${message.guild.id}.json`)
     if ((args[0]) && (args[1]) && (args[2]) && (exploiterReportChannel)) {
+      let url = args[1]
+      url = url.replace('studio.youtube.com/video/','www.youtube.com/watch?v=')
+      url = url.replace('/edit','')
       const reason = args.slice(2).join(' ')
       const response = await request(`https://api.roblox.com/users/get-by-username?username=${args[0]}`, { validateStatus: false })
       if (response.data.Username) {
         const urltest = await request(args[1], { validateStatus: false })
         if (!urltest || urltest.status !== 200) return message.channel.send('This url is either not valid or cannot be verified.')
         const bancheck = await request(`https://storage.googleapis.com/${config.bucket}/${response.data.Id}.json`, { validateStatus: false })
-        let description = `${message.author.username}#${message.author.discriminator} has reported ${response.data.Username} for exploiting!\n\nReason: ${args[2]}\n\n[Evidence](${reason})`
+        let description = `${message.author.username}#${message.author.discriminator} has reported ${response.data.Username} for exploiting!\n\nReason: ${reason}\n\n[Evidence](${url})`
         if (bancheck.status === 200 && bancheck.data.usercode === '0x1') description += '\n\nThis user is blacklisted!'
         else if (bancheck.status === 200 && bancheck.data.usercode === '0x2') description += '\n\nThis user is banned!'
         const embed = new Discord.MessageEmbed()
