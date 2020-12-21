a4c1bda85e89e25e5c3b44d3ad18da508cfdfce0
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-20 19:51:07 -0500

    Finish group rank binds

--- verify.js
+++ verify.js
@@ -13,19 +13,36 @@ module.exports = {
     }
     if (linkedRoles.rowCount === 0) return message.channel.send('User has been verified')
     const member = await message.guild.members.fetch(user)
+    let groupdata = await request(`https://groups.roblox.com/v1/users/${robloxId}/groups/roles`, { validateStatus: false })
     linkedRoles.rows.forEach(async row => {
-      const roledata = await request(`https://inventory.roblox.com/v1/users/${robloxId}/items/${row.type}/${row.item_id}`).catch((e) => {
-        console.error(e)
-        return message.channel.send('I could not look up roles! Roblox appears to be having issues.')
-      })
-      if (roledata.data && roledata.data !== []) {
-        if (message.guild.roles.cache.find(role => role.id === row.role_id)) {
-          const role = message.guild.roles.cache.find(role => role.id === row.role_id)
-          if (role.position > message.guild.me.roles.highest.position) member.roles.add(row.role_id).catch(e => console.error(e))
-        } else {
-          return message.channel.send('I could not finish giving the roles! One of the set roles was deleted!')
+      if (row.type !== 'Group') {
+        const roledata = await request(`https://inventory.roblox.com/v1/users/${robloxId}/items/${row.type}/${row.item_id}`).catch((e) => {
+          console.error(e)
+          return message.channel.send('I could not look up roles! Roblox appears to be having issues.')
+        })
+        if (roledata.data && roledata.data !== []) {
+          if (message.guild.roles.cache.find(role => role.id === row.role_id)) {
+            const role = message.guild.roles.cache.find(role => role.id === row.role_id)
+            if (role.position > message.guild.me.roles.highest.position) member.roles.add(row.role_id).catch(e => console.error(e))
+          } else {
+            return message.channel.send('I could not finish giving the roles! One of the set roles was deleted!')
+          }
         }
+      } else {
+        if (!groupdata.data || groupdata.data.data === []) return
+        groupdata = groupdata.data.data
+        linkedRoles.rows.forEach(async role => {
+          for (let i = 0; i < groupdata.length; i++) {
+            if (role.rank === groupdata[i].role.rank) {
+              const boundrole = message.guild.roles.cache.find(item => role.role_id === item.id)
+              if (boundrole) {
+                if (boundrole.position > message.guild.me.roles.highest.position) member.roles.add(role.role_id).catch(e => console.error(e))
+              }
+            }
+          }
+        })
       }
     })
+    return message.channel.send('Verification finished!')
   }
 }
