3a36b320c505b44849b24978539abc07a7f54d55
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-22 18:36:50 -0500

    Insert user if it doesn't exist

--- commands/AppealBanCommand.js
+++ commands/AppealBanCommand.js
@@ -9,10 +9,11 @@ module.exports = {
     if (!message.member.roles.cache.some(role => config.appealsManagerRole.includes(role.id))) {
       return message.channel.send('You do not have permission to run this command!')
     }
+    if (!args[0]) return message.channel.send('You did not specify a user!')
     try {
       const user = await db.query('SELECT * FROM auth WHERE discord_id = $1;', [args[0]])
-      if (!user) {
-        return message.channel.send('This user could not be found!')
+      if (user.rowCount === 0) {
+        await db.query('INSERT INTO auth(discord_id,email,username,discriminator,blocked) VALUES($1,$2,$3,$4,$5);', [args[0], 'removed', 'unknown', 0000, true])
       }
       await db.query('UPDATE auth SET blocked = true WHERE discord_id = $1;', [args[0]])
       message.channel.send('User has been banned from the form!')
