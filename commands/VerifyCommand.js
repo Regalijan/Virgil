33dc3110cb2a0f9444b458568988d24848414d92
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-23 10:51:50 -0500

    Fix message sometimes not updating at the end

--- commands/VerifyCommand.js
+++ commands/VerifyCommand.js
@@ -6,6 +6,6 @@ module.exports = {
   guildOnly: true,
   async execute (message) {
     const status = await verifier.run(message, message.author.id)
-    if (status.content === ':scroll: Fetching information from Roblox...') return status.edit('Verification Complete!')
+    if (status.content === ':scroll: Fetching information from Roblox...' || ':scroll: Checking the verification registries...') status.edit('Verification Complete!')
   }
 }
