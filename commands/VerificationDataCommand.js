cab000ac4218593e57e669079d791e7aee033c7a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-24 14:34:59 -0500

    Use entire body response

--- commands/VerificationDataCommand.js
+++ commands/VerificationDataCommand.js
@@ -4,17 +4,13 @@ module.exports = {
   name: 'verificationdata',
   description: 'Displays your verification data from the RoVer and BloxLink registries',
   async execute (message) {
-    let roverData = await request(`https://verify.eryn.io/api/user/${message.author.id}`, { validateStatus: false })
-    if (roverData.status !== 200) roverData = 'No data'
-    else roverData = `\`{"robloxUsername":"${roverData.data.robloxUsername}","robloxId": ${roverData.data.robloxId}}\``
-    let bloxlinkData = await request(`https://api.blox.link/v1/user/${message.author.id}`, { validateStatus: false })
-    if (bloxlinkData.status !== 200 || bloxlinkData.data.status !== 'ok') bloxlinkData = 'No data'
-    else bloxlinkData = `\`{"primaryAccount":"${bloxlinkData.data.primaryAccount}"}\``
+    const roverData = await request(`https://verify.eryn.io/api/user/${message.author.id}`, { transformResponse: [], validateStatus: false })
+    const bloxlinkData = await request(`https://api.blox.link/v1/user/${message.author.id}`, { transformResponse: [], validateStatus: false })
     const embed = new MessageEmbed()
       .setTitle('Verification Data')
       .addFields(
-        { name: 'RoVer', value: `https://verify.eryn.io/api/user/${message.author.id}:\n${roverData}` },
-        { name: 'BloxLink', value: `https://api.blox.link/v1/user/${message.author.id}:\n${bloxlinkData}` }
+        { name: 'RoVer', value: `https://verify.eryn.io/api/user/${message.author.id}:\n\`${roverData.data}\`` },
+        { name: 'BloxLink', value: `https://api.blox.link/v1/user/${message.author.id}:\n\`${bloxlinkData.data}\`` }
       )
       .setColor(3756250)
     message.channel.send(embed)
