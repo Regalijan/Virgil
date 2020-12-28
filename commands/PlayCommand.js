e7db2edaf9577c92e1e0cef4f02e0e1f2aea6916
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-28 01:48:34 -0500

    Remove all headers

--- commands/PlayCommand.js
+++ commands/PlayCommand.js
@@ -14,12 +14,14 @@ module.exports = {
         const connection = await message.member.voice.channel.join()
         const addToQueueQuery = 'INSERT INTO music_queue(time,requester,media,guild,title) VALUES($1,$2,$3,$4,$5) RETURNING *;'
         const ts = parseInt(Date.now().toString().concat(Math.round(Math.random() * 1000000).toString()))
-        const accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        /*
+        const accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,star/star;q=0.8,application/signed-exchange;v=b3;q=0.9'
         const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.459'
         const lang = 'en-US,en;q=0.9'
         const referer = 'https://www.google.com'
         const enc = 'gzip, deflate, br'
         const uis = 1
+        */
         async function playTrack () {
           try {
             const queue = await db.query(`SELECT * FROM music_queue WHERE guild = ${message.guild.id};`)
@@ -28,7 +30,7 @@ module.exports = {
               return message.channel.send('All tracks have finished.')
             }
             message.channel.send(`**Now playing ${queue.rows[0].title}**`)
-            const dispatcher = connection.play(ytdl(queue.rows[0].media, { requestOptions: { headers: { Accept: accept, 'User-Agent': ua, 'Accept-Language': lang, 'Accept-Encoding': enc, Referer: referer } } }))
+            const dispatcher = connection.play(ytdl(queue.rows[0].media))
             dispatcher.on('finish', () => {
               db.query(`DELETE FROM music_queue WHERE time = ${queue.rows[0].time.toString()};`).catch(e => console.error(e))
               dispatcher.destroy()
