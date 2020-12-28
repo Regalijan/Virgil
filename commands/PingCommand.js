7bb1fe612b8e65c188a9da928b0917a0102968b5
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-27 19:03:26 -0500

    Forgot ms indicator

--- commands/PingCommand.js
+++ commands/PingCommand.js
@@ -8,6 +8,6 @@ module.exports = {
     const beginningTS = Date.now()
     const ping = await message.channel.send(':bulb: Checking...')
     await db.query('SELECT * FROM core_settings WHERE guild_id = $1', [message.guild.id])
-    ping.edit(`:ping_pong: WebSocket: ${client.ws.ping}ms\nMessage Latency: ${ping.createdTimestamp - message.createdTimestamp}ms\nRound Trip: ${Date.now() - beginningTS}`)
+    ping.edit(`:ping_pong: WebSocket: ${client.ws.ping}ms\nMessage Latency: ${ping.createdTimestamp - message.createdTimestamp}ms\nRound Trip: ${Date.now() - beginningTS}ms`)
   }
 }
