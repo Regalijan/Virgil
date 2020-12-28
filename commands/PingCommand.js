0952d1c4852d2eaa82f8fd86c51cb1fb813f4301
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2020-12-27 19:00:12 -0500

    Add db test to ping

--- commands/PingCommand.js
+++ commands/PingCommand.js
@@ -1,10 +1,13 @@
+const beginningTS = Date.now()
 const { client } = require('../index')
+const db = require('../database')
 
 module.exports = {
   name: 'ping',
   description: 'Pong',
   async execute (message) {
     const ping = await message.channel.send(':bulb: Checking...')
-    ping.edit(`:ping_pong: WebSocket: ${client.ws.ping}ms, Total: ${ping.createdTimestamp - message.createdTimestamp}ms`)
+    await db.query('SELECT * FROM core_settings WHERE guild_id = $1', [message.guild.id])
+    ping.edit(`:ping_pong: WebSocket: ${client.ws.ping}ms\nMessage Latency: ${ping.createdTimestamp - message.createdTimestamp}ms\nRound Trip: ${Date.now() - beginningTS}`)
   }
 }
