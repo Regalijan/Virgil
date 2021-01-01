a8232724ccf7630f5b68b75c38b7bcabb576c351
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-01 01:43:57 -0500

    Add permission and arg check

--- commands/UpdateCommand.js
+++ commands/UpdateCommand.js
@@ -5,6 +5,8 @@ module.exports = {
   description: "Updates the target user's role bindings.",
   guildOnly: true,
   async execute (message, args) {
+    if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You do not have permission to run this command!')
+    if (!args[0]) return message.channel.send('No member was specified.')
     const status = await verifier.run(message, args[0])
     if (status === 1) {
       const member = await message.guild.members.fetch(args[0])
