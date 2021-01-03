0e777f724074b25cd51ffc807024d42be40d55a5
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-02 22:45:00 -0500

    JSON files bad lol (also add voice logging)

--- index.js
+++ index.js
@@ -35,9 +35,11 @@ client.on('message', async message => {
 
 client.on('guildMemberAdd', async member => {
   try {
-    const serversettings = require(`./serversettings/${member.guild.id}.json`)
-    if (!serversettings.joinLogChannel) return
-    const channel = member.guild.channels.cache.find(ch => ch.id === serversettings.joinLogChannel)
+    let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [member.guild.id])
+    serversettings = serversettings.rows[0]
+    if (!serversettings.join_log_channel) return
+    const channel = member.guild.channels.cache.find(ch => ch.id === serversettings.join_log_channel.toString())
+    if (!channel) return
     const embed = new Discord.MessageEmbed()
       .setAuthor('Member Joined', member.user.displayAvatarURL())
       .setDescription(`${member} ${member.user.tag}`)
@@ -60,9 +62,11 @@ client.on('guildMemberAdd', async member => {
 
 client.on('guildMemberRemove', async member => {
   try {
-    const serversettings = require(`./serversettings/${member.guild.id}.json`)
-    if (!serversettings.joinLogChannel) return
-    const channel = member.guild.channels.cache.find(ch => ch.id === serversettings.joinLogChannel)
+    let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [member.guild.id])
+    serversettings = serversettings.rows[0]
+    if (!serversettings.join_log_channel) return
+    const channel = member.guild.channels.cache.find(ch => ch.id === serversettings.join_log_channel.toString())
+    if (!channel) return
     const embed = new Discord.MessageEmbed()
       .setAuthor('Member Left', member.user.displayAvatarURL())
       .setDescription(`${member} ${member.user.username}#${member.user.discriminator}`)
@@ -77,9 +81,11 @@ client.on('guildMemberRemove', async member => {
 
 client.on('guildBanAdd', async (guild, user) => {
   try {
-    const serversettings = require(`./serversettings/${guild.id}.json`)
-    if (!serversettings.banLogChannel) return
-    const channel = guild.channels.cache.find(ch => ch.id === serversettings.banLogChannel)
+    let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [guild.id])
+    serversettings = serversettings.rows[0]
+    if (!serversettings.ban_log_channel) return
+    const channel = guild.channels.cache.find(ch => ch.id === serversettings.ban_log_channel.toString())
+    if (!channel) return
     const embed = new Discord.MessageEmbed()
       .setAuthor('Member Banned', user.displayAvatarURL())
       .setDescription(`${user} ${user.tag}`)
@@ -94,9 +100,11 @@ client.on('guildBanAdd', async (guild, user) => {
 
 client.on('guildBanRemove', async (guild, user) => {
   try {
-    const serversettings = require(`./serversettings/${guild.id}.json`)
-    if (!serversettings.banLogChannel) return
-    const channel = guild.channels.cache.find(ch => ch.id === serversettings.banLogChannel)
+    let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [guild.id])
+    serversettings = serversettings.rows[0]
+    if (!serversettings.ban_log_channel) return
+    const channel = guild.channels.cache.find(ch => ch.id === serversettings.ban_log_channel.toString())
+    if (!channel) return
     const embed = new Discord.MessageEmbed()
       .setAuthor('Member Unbanned', user.displayAvatarURL())
       .setDescription(`${user} ${user.tag}`)
@@ -113,9 +121,13 @@ client.on('messageDelete', async message => {
   if (message.channel.type === 'dm') return
   if (message.author.bot) return
   try {
-    const serversettings = require(`./serversettings/${message.guild.id}.json`)
-    if ((!serversettings.deleteLogChannel) || (message.author.id === client.user.id) || (serversettings.ignoredChannels.includes(message.channel.id)) || (serversettings.ignoredCategories.includes(message.channel.parent.id))) return
-    const channel = message.guild.channels.cache.find(ch => ch.id === serversettings.deleteLogChannel)
+    const snowflakecheck = await db.query('SELECT * FROM ignored WHERE snowflake = $1 OR snowflake = $2;', [message.channel.id, message.channel.parent.id])
+    if (snowflakecheck.rowCount === 0) return
+    let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [message.guild.id])
+    serversettings = serversettings.rows[0]
+    if (!serversettings.delete_log_channel) return
+    const channel = message.guild.channels.cache.find(ch => ch.id === serversettings.delete_log_channel.toString())
+    if (!channel) return
     let auditlogs = await message.guild.fetchAuditLogs({ limit: 1, type: 72 })
     auditlogs = auditlogs.entries.first()
     let messagecontent = `Message ${message.id} deleted from ${message.channel}`
@@ -144,9 +156,11 @@ client.on('messageDelete', async message => {
 client.on('messageUpdate', async (oldMessage, newMessage) => {
   if ((oldMessage.content) && (newMessage.content) && (newMessage.channel.type !== 'dm') && (!newMessage.author.bot) && (oldMessage.content !== newMessage.content)) {
     try {
-      const serversettings = require(`./serversettings/${newMessage.guild.id}.json`)
-      if ((!serversettings.editLogChannel) || (oldMessage.channel.type === 'dm') || (serversettings.ignoredChannels.includes(newMessage.channel.id)) || (serversettings.ignoredCategories.includes(newMessage.channel.parent.id))) return
-      const channel = oldMessage.guild.channels.cache.find(ch => ch.id === serversettings.editLogChannel)
+      const snowflakecheck = await db.query('SELECT * FROM ignored WHERE snowflake = $1 OR snowflake = $2;', [newMessage.channel.id, newMessage.channel.parent.id])
+      if (snowflakecheck.rowCount === 0) return
+      let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [newMessage.guild.id])
+      serversettings = serversettings.rows[0]
+      const channel = oldMessage.guild.channels.cache.find(ch => ch.id === serversettings.edit_log_channel.toString())
       const embed = new Discord.MessageEmbed()
         .setAuthor(`${oldMessage.author.username}#${oldMessage.author.discriminator} (${oldMessage.author.id})`, oldMessage.author.displayAvatarURL())
         .setDescription(`**Message edited in** ${oldMessage.channel} [Go to message](${oldMessage.url})`)
@@ -161,30 +175,45 @@ client.on('messageUpdate', async (oldMessage, newMessage) => {
 })
 
 client.on('messageDeleteBulk', async (messages) => {
-  let serversettings
-  messages.findKey(m => { serversettings = require(`./serversettings/${m.guild.id}.json`) })
+  let channel
+  messages.findKey(m => { channel = m.channel })
+  const serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [channel.guild.id])
+  const ignorecheck = await db.query('SELECT * FROM ignored WHERE snowflake = $1 OR snowflake = $2;', [channel.id, channel.parent.id])
+  if (ignorecheck.rowCount === 0) return
+  if (!serversettings.delete_log_channel) return
   let contents = `BULK DELETE - ${Date()}`
   messages.each(m => { contents += `\n\n[${m.author.id}](${m.author.tag}) ${m.createdAt}: ${m.content}` })
-  let originatingChannel
-  messages.findKey(m => { originatingChannel = m.channel })
-  if (serversettings.ignoredChannels.includes(originatingChannel.id) || serversettings.ignoredCategories.includes(originatingChannel.parentID)) return
   const fileName = `./bulk-${crypto.randomBytes(16).toString('hex')}.txt`
   try {
     fs.writeFileSync(fileName, contents)
   } catch (e) {
     return console.error(e)
   }
-  if ((!serversettings.deleteLogChannel) || (serversettings.ignoredCategories.includes(messages.findKey(m => { return m.channel.parentID }))) || (serversettings.ignoredChannels.includes(messages.findKey(m => { return m.channel.id })))) return
-  let channel
-  messages.findKey(m => { channel = m.guild.channels.resolve(serversettings.deleteLogChannel) })
+  const logchannel = channel.guild.chnnels.cache.find(ch => ch.id === serversettings.delete_log_channel.toString())
+  if (!logchannel) return
   const file = new Discord.MessageAttachment(fileName)
   const embed = new Discord.MessageEmbed()
     .setAuthor('Bulk Delete')
     .setTitle('Virgil Message Logging')
-    .setDescription(`Bulk delete for ${originatingChannel}`)
+    .setDescription(`Bulk delete for ${channel}`)
     .attachFiles(file)
-  await channel.send(embed)
+  await logchannel.send(embed)
   fs.unlink(fileName, err => { if (err) return console.error(err) })
+})
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  let serversettings = await db.query('SELECT * FROM core_settings WHERE guild_id = $1;', [newState.guild.id])
+  serversettings = serversettings.rows[0]
+  if (!serversettings.voice_log_channel) return
+  let change
+  if (oldState.channel) change = `left ${oldState.channel.name}`
+  else change = `joined ${newState.channel.name}`
+  const channel = newState.guild.channels.cache.find(ch => ch.id === serversettings.voice_log_channel.toString())
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(newState.member.user.tag, newState.member.user.displayAvatarURL())
+    .setDescription(`${newState.member} ${change}`)
+    .setFooter(`ID: ${newState.member.id}`)
+  await channel.send(embed).catch(e => console.error(e))
 })
 
 client.on('invalidated', () => {
