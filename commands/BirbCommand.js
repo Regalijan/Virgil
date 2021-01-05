41f5eabac911127bd0511c1e2c76b5f0714f9bc1
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-05 16:10:15 -0500

    Create birb command

---
+++ commands/BirbCommand.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'birb',
+  description: 'Finds image of birb',
+  async execute (message) {
+    const request = require('axios')
+    const imglist = await request('https://random.birb.pw/img/', { responseType: 'text' })
+    const imagematches = imglist.data.match(/\/img\/\S[^.<]*\.[A-z]*/g)
+    const selectedindex = Math.round(Math.random() * (imagematches.length - 1))
+    const { MessageEmbed } = require('discord.js')
+    const embed = new MessageEmbed()
+      .setTitle('Tweet Tweet...')
+      .setImage(`https://random.birb.pw${imagematches[selectedindex]}`)
+    await message.channel.send(embed)
+  }
+}
