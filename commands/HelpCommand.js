c3ee97eb3dd2bf863b5b3a0435ed3774794e04e2
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-15 14:21:31 -0500

    Help command

---
+++ commands/HelpCommand.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'help',
+  description: 'Displays all commands',
+  async execute (message, args) {
+    const { prefix } = require('../config.json')
+    const { MessageEmbed } = require('discord.js')
+    const embed = new MessageEmbed()
+      .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    message.client.commands.forEach(async cmd => {
+      embed.addField(`${prefix}cmd.name`, cmd.description)
+    })
+    await message.channel.send(embed)
+  }
+}
