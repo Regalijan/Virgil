471a2435f9a6bfc22b74cc451384b3f2de8fbb35
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-16 22:40:07 -0500

    Create disabledeletelog command

---
+++ commands/DisableDeleteLogCommand.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'disabledeletelog',
+  description: 'Disables delete logging',
+  guildOnly: true,
+  async execute (message, args) {
+    if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You do not have permission to run this command!')
+    const db = require('../database')
+    await db.query('UPDATE core_settings SET delete_log_channel = null WHERE guild_id = $1;', [message.guild.id]).catch(e => {
+      console.error(e)
+      return message.channel.send('An error occured when disabling delete logs!')
+    })
+    message.channel.send('Delete logging disabled!')
+  }
+}
