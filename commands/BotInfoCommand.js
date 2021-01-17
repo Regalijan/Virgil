6cfc6120a38b6328bc247050839b5d202c25e3ed
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-16 19:40:38 -0500

    Make better looking strings for uptime fields

--- commands/BotInfoCommand.js
+++ commands/BotInfoCommand.js
@@ -15,6 +15,28 @@ module.exports = {
       console.error(e)
       return message.channel.send('An error occured when fetching commit information!')
     }
+    let sysuptime = os.uptime()
+    let days = Math.floor(sysuptime / 86400)
+    if (days > 0) sysuptime %= 86400
+    let hours = Math.floor(sysuptime / 3600)
+    if (hours > 0) sysuptime %= 3600
+    let minutes = Math.floor(sysuptime / 60)
+    if (minutes > 0) sysuptime %= 60
+    let sysuptimestr = `${sysuptime}s`
+    if (minutes > 0) sysuptimestr += `${minutes}m `
+    if (hours > 0) sysuptimestr += `${hours}h `
+    if (days > 0) sysuptimestr += `${days}d `
+    let botuptime = Math.floor(client.uptime / 1000)
+    days = Math.floor(botuptime / 86400)
+    if (days > 0) botuptime %= 86400
+    hours = Math.floor(botuptime / 3600)
+    if (hours > 0) botuptime %= 3600
+    minutes = Math.floor(botuptime / 60)
+    if (minutes > 0) botuptime %= 60
+    let botuptimestr = `${botuptime}s`
+    if (minutes > 0) botuptimestr += `${minutes}m `
+    if (hours > 0) botuptimestr += `${hours}h `
+    if (days > 0) botuptimestr += `${days}`
     const embed = new MessageEmbed()
       .setAuthor(client.user.tag, client.user.displayAvatarURL())
       .setTitle('System Information')
@@ -27,9 +49,9 @@ module.exports = {
         { name: 'Operating System', value: os.version() },
         { name: 'CPU Cores (logical)', value: cpus.length },
         { name: 'Processor', value: cpus[0].model },
-        { name: 'Total Memory', value: `${os.totalmem() / 1000000} MB` },
-        { name: 'System Uptime', value: `${os.uptime()} seconds` },
-        { name: 'Process Uptime', value: `${Math.floor(client.uptime / 1000)} seconds` },
+        { name: 'Total Memory', value: `${Math.floor(os.totalmem() / 1000000)} MB` },
+        { name: 'System Uptime', value: sysuptimestr },
+        { name: 'Process Uptime', value: botuptimestr },
         { name: 'Node Version', value: process.version }
       )
     await message.channel.send(embed).catch(() => {})
