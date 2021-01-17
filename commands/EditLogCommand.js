a9d14219d1abc63ba456aa532f61aa4a2b64958a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-16 22:21:18 -0500

    Create editlog command

---
+++ commands/EditLogCommand.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: 'editlog',
+  description: 'Sets edit log channel',
+  guildOnly: true,
+  async execute (message, args) {
+    if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You do not have permission to run this command!')
+    const { prefix } = require('../config.json')
+    if (args.length === 0) return message.channel.send(`Usage: \`${prefix}editlog #channel\`\nThe channel can be a tag or an id.`)
+    const chosenchannel = args[0].replace(/<#|>/g, '')
+    const channel = message.guild.channels.cache.find(c => c.id === chosenchannel)
+    if (!channel) return message.channel.send('I could not find that channel!')
+    const db = require('../database')
+    await db.query('UPDATE core_settings SET edit_log_channel = $1 WHERE guild_id = $2;', [chosenchannel, message.guild.id]).catch(e => {
+      console.error(e)
+      return message.channel.send('An error occured when setting the edit log channel!')
+    })
+    message.channel.send(`Edit logging channel set to ${channel}!`)
+  }
+}
