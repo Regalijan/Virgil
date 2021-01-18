6634126d3a2c96b95f92bb3a19b0a839d8797003
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-18 16:07:35 -0500

    Circular dependencies much?

--- commands/ExitCommand.js
+++ commands/ExitCommand.js
@@ -1,8 +1,8 @@
-const { app } = require('../index')
 module.exports = {
   name: 'exit',
   description: 'Exits the bot process',
   async execute (message) {
+    const { app } = require('../index')
     if (message.author.id === app.owner.id) {
       await message.channel.send('Bye.')
       message.client.destroy()
