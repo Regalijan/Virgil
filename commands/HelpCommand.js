befe96b9235879492a45fa584f663e074e6f2893
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-18 01:24:16 -0500

    Split messages in help command

--- commands/HelpCommand.js
+++ commands/HelpCommand.js
@@ -3,12 +3,10 @@ module.exports = {
   description: 'Displays all commands',
   async execute (message, args) {
     const { prefix } = require('../config.json')
-    const { MessageEmbed } = require('discord.js')
-    const embed = new MessageEmbed()
-      .setAuthor(message.author.tag, message.author.displayAvatarURL())
-    message.client.commands.forEach(async cmd => {
-      embed.addField(`${prefix}${cmd.name}`, cmd.description)
+    let cmds = '```Commands```'
+    message.client.commands.forEach(cmd => {
+      cmds += `\n${prefix}${cmd.name} - ${cmd.description}`
     })
-    await message.channel.send(embed)
+    await message.channel.send(cmds, { split: true })
   }
 }
