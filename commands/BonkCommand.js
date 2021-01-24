c463ab55e0e6d58171496e1b306fc0188c4e1623
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-24 04:27:17 -0500

    Fix bonk command a bit

--- commands/BonkCommand.js
+++ commands/BonkCommand.js
@@ -5,12 +5,12 @@ module.exports = {
   async execute (message, args) {
     const { getuser } = require('../getuser')
     const { MessageEmbed } = require('discord.js')
-    const query = args.slice(0).join(' ')
+    if (args.length === 0) return await message.channel.send('You did not choose someone to bonk!')
     const target = await getuser(query, message)
     const embed = new MessageEmbed()
       .setTitle('BONK!')
       .setImage('https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg')
-      .setDescription(`<@${message.author.id}> has bonked ${target}.`)
+      .setDescription(`${message.member} has bonked ${target}.`)
       .setColor(3756250)
     message.channel.send(embed)
   }
