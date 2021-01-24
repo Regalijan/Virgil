1512f87c9a4c2458044476518201cbff2598d0a1
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-24 04:24:08 -0500

    how did i miss this like seriously

--- commands/NoobDetectorCommand.js
+++ commands/NoobDetectorCommand.js
@@ -3,7 +3,8 @@ module.exports = {
   description: 'Find out how much of a noob you are',
   async execute (message, args) {
     const { getuser } = require('../getuser')
-    let member = await getuser(args.slice(0).join(' '), message)
+    let member = message.member
+    if (args.length > 0) member = await getuser(args.slice(0).join(' '), message)
     if (!member) member = message.member
     const { MessageEmbed } = require('discord.js')
     const embed = new MessageEmbed()
