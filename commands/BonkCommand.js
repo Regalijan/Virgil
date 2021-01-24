c45a459bd502bd33dba4db1c7ce87e0af1a9f750
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-24 02:46:30 -0500

    Fix bonk command finally

--- commands/BonkCommand.js
+++ commands/BonkCommand.js
@@ -1,15 +1,13 @@
-const Discord = require('discord.js')
-
 module.exports = {
   name: 'bonk',
   description: 'BONK!',
   guildOnly: true,
-  execute (message, args) {
-    let target = `<@${args[0]}>`
-    if (args[0].match(/(^<@!?[0-9]*>)/)) {
-      target = message.mentions.members.first()
-    }
-    const embed = new Discord.MessageEmbed()
+  async execute (message, args) {
+    const { getuser } = require('../getuser')
+    const { MessageEmbed } = require('discord.js')
+    const query = args.slice(0).join(' ')
+    const target = await getuser(query, message)
+    const embed = new MessageEmbed()
       .setTitle('BONK!')
       .setImage('https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg')
       .setDescription(`<@${message.author.id}> has bonked ${target}.`)
