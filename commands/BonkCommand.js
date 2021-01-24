e59459a854d07a4994e266f249763fc23061c98b
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-01-24 04:29:46 -0500

    fix undefined param

--- commands/BonkCommand.js
+++ commands/BonkCommand.js
@@ -6,7 +6,7 @@ module.exports = {
     const { getuser } = require('../getuser')
     const { MessageEmbed } = require('discord.js')
     if (args.length === 0) return await message.channel.send('You did not choose someone to bonk!')
-    const target = await getuser(query, message)
+    const target = await getuser(args.slice(0).join(' '), message)
     const embed = new MessageEmbed()
       .setTitle('BONK!')
       .setImage('https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg')
