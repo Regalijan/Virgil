996f8de7d401c46db0ca4b27b1a08baf848f91c4
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-02-02 01:12:30 -0500

    Add special description for someone who bonks themself

--- commands/BonkCommand.js
+++ commands/BonkCommand.js
@@ -6,11 +6,14 @@ module.exports = {
     const { getuser } = require('../getuser')
     const { MessageEmbed } = require('discord.js')
     if (args.length === 0) return await message.channel.send('You did not choose someone to bonk!')
-    const target = await getuser(args.slice(0).join(' '), message)
+    let target = await getuser(args.slice(0).join(' '), message)
+    if (!target) target = message.member
+    let description = `${message.member} has bonked ${target}.`
+    if (message.member.id === target.id) description = `${message.member} managed to bonk themselves!`
     const embed = new MessageEmbed()
       .setTitle('BONK!')
       .setImage('https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg')
-      .setDescription(`${message.member} has bonked ${target}.`)
+      .setDescription(description)
       .setColor(3756250)
     message.channel.send(embed)
   }
