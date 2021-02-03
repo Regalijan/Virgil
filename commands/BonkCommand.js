c98f75958ba0bed4624075caa67dc810e3860c50
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-02-03 17:57:32 -0500

    No allowy bonky owner

--- commands/BonkCommand.js
+++ commands/BonkCommand.js
@@ -8,9 +8,12 @@ module.exports = {
     if (args.length === 0) return await message.channel.send('You did not choose someone to bonk!')
     let target = await getuser(args.slice(0).join(' '), message)
     if (!target) target = message.member
+    const app = require('../index')
+    const owner = (await app).owner.id
     let description = `${message.member} has bonked ${target}.`
     if (message.member.id === target.id) description = `${message.member} managed to bonk themselves!`
     else if (target.id === message.client.user.id) return await message.channel.send('You will *not* bonk me.')
+    else if (target.id === owner && message.author.id !== owner) return await message.channel.send('I will not let you bonk my owner.')
     const embed = new MessageEmbed()
       .setTitle('BONK!')
       .setImage('https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg')
