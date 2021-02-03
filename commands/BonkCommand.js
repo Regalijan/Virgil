20d7224861fea889cdec06ab3940bff125c8546f
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-02-03 14:32:24 -0500

    You will *not* bonk me

--- commands/BonkCommand.js
+++ commands/BonkCommand.js
@@ -10,6 +10,7 @@ module.exports = {
     if (!target) target = message.member
     let description = `${message.member} has bonked ${target}.`
     if (message.member.id === target.id) description = `${message.member} managed to bonk themselves!`
+    else if (target.id === message.client.user.id) return await message.channel.send('You will *not* bonk me.')
     const embed = new MessageEmbed()
       .setTitle('BONK!')
       .setImage('https://i.pinimg.com/originals/f7/30/3b/f7303b16c4d7902e88060de1ad3c9ed3.jpg')
