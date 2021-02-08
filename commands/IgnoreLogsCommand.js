5a284aebe594f8736a1ef87fa891360423378218
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-02-08 11:31:37 -0500

    Create ignorelogs command

---
+++ commands/IgnoreLogsCommand.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'ignorelogs',
+  description: 'Ignores a channel (logging)',
+  guildOnly: true,
+  async execute (message, args) {
+    if (!message.member.hasPermission('MANAGE_GUILD')) return await message.channel.send('You cannot run this command!')
+    if (args.length === 0) {
+      const { prefix } = require('../config.json')
+      return await message.channel.send(`Usage: \`${prefix}ignorelogs <channel/categoryID>\`\nCategories must be supplied as an ID!`)
+    }
+    const channel = message.guild.channels.cache.find(c => c.id === args[0].replace(/<#|>/g, ''))
+    if (!channel) return await message.channel.send('I could not find that channel or category!')
+    const db = require('../database')
+    await db.query('INSERT INTO ignored(snowflake,type,guild) VALUES($1,$2,$3);', [channel.id, channel.type, message.guild.id]).catch(async () => await message.channel.send('An error occured when ignoring!'))
+    await message.channel.send(`${channel} ignored from logs!`)
+  }
+}
