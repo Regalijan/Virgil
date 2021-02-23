cf3a4fd5eafd758d8b313289df336870d6da288a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-02-23 00:25:36 -0500

    Remove references to bloxlink in reverify

--- commands/ReverifyCommand.js
+++ commands/ReverifyCommand.js
@@ -3,6 +3,6 @@ module.exports = {
   description: 'Sends links to change your verified account(s)',
   guildOnly: true,
   async execute (message) {
-    message.reply('To change your verified account, please visit <https://verify.eryn.io> or <https://blox.link/verify> if you have verified on BloxLink but not RoVer.')
+    message.reply('To change your verified account, please visit <https://verify.eryn.io>.')
   }
 }
