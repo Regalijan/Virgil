869f12542a4f7fb59c2a8df0c3fbefc109420ac7
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-03-05 00:55:20 -0500

    Create cat command

---
+++ commands/CatCommand.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'cat',
+  description: 'Gets cat image',
+  async execute (message) {
+    const request = require('axios').default
+    const cat = await request('https://nekos.life/api/v2/img/meow').catch(() => {})
+    if (!cat) return await message.channel.send('An error occured :(')
+    const { MessageEmbed } = require('discord.js')
+    const embed = new MessageEmbed()
+      .setTitle('Meow :cat:')
+      .setColor(3756250)
+      .setImage(cat.data.url)
+      .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    await message.channel.send(embed).catch(() => {})
+  }
+}
--- commands/OwoifyCommand.js
+++ commands/OwoifyCommand.js
@@ -4,7 +4,7 @@ module.exports = {
   async execute (message, args) {
     if (args.slice(0).join(' ') > 200) return await message.channel.send('Text must be 200 characters or less!')
     const body = encodeURIComponent(args.slice(0).join(' '))
-    const request = require('axios').default
+    const request = require('axios')
     const owo = await request(`https://nekos.life/api/v2/owoify?text=${body}`).catch(() => {})
     if (!owo) return await message.channel.send('An error occured :(')
     await message.channel.send(owo.data.owo)
