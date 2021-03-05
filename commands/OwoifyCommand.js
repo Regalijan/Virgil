f08920a77b49880b8cb562c12378ef4bcb42130f
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-03-05 10:30:35 -0500

    Check for text of zero length

--- commands/OwoifyCommand.js
+++ commands/OwoifyCommand.js
@@ -2,6 +2,7 @@ module.exports = {
   name: 'owoify',
   description: 'OwOifies text (200 characters maximum)',
   async execute (message, args) {
+    if (args.length === 0) return await message.channel.send('I need some text first!')
     if (args.slice(0).join(' ') > 200) return await message.channel.send('Text must be 200 characters or less!')
     const body = encodeURIComponent(args.slice(0).join(' '))
     const request = require('axios')
