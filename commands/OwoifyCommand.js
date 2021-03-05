1c4fd7e46b76c3a0d59d30b296de0b54f860e2b6
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-03-05 10:38:57 -0500

    Add missing .length

--- commands/OwoifyCommand.js
+++ commands/OwoifyCommand.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'OwOifies text (200 characters maximum)',
   async execute (message, args) {
     if (args.length === 0) return await message.channel.send('I need some text first!')
-    if (args.slice(0).join(' ') > 200) return await message.channel.send('Text must be 200 characters or less!')
+    if (args.slice(0).join(' ').length > 200) return await message.channel.send('Text must be 200 characters or less!')
     const body = encodeURIComponent(args.slice(0).join(' '))
     const request = require('axios')
     const owo = await request(`https://nekos.life/api/v2/owoify?text=${body}`).catch(() => {})
