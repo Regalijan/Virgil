7683175a6cad2d08b2e18a360ed6141673d16bb0
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-03-06 00:39:04 -0500

    Perform owoifying locally

--- commands/OwoifyCommand.js
+++ commands/OwoifyCommand.js
@@ -3,11 +3,13 @@ module.exports = {
   description: 'OwOifies text (200 characters maximum)',
   async execute (message, args) {
     if (args.length === 0) return await message.channel.send('I need some text first!')
-    if (args.slice(0).join(' ').length > 200) return await message.channel.send('Text must be 200 characters or less!')
-    const body = encodeURIComponent(args.slice(0).join(' '))
-    const request = require('axios')
-    const owo = await request(`https://nekos.life/api/v2/owoify?text=${body}`).catch(() => {})
-    if (!owo) return await message.channel.send('An error occured :(')
-    await message.channel.send(owo.data.owo)
+    const body = args.slice(0).join(' ')
+      .replace(/[lr]/g, 'w')
+      .replace(/[LR]/g, 'W')
+      .replace(/n([aeiou])/g, 'ny$1')
+      .replace(/N([aeiou])/g, 'Ny$1')
+      .replace(/N([AEIOU])/g, 'Ny$1')
+      .replace(/ove/g, 'uv')
+    await message.channel.send(body)
   }
 }
