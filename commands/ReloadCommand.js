51838b491ad2ae634e806eedec1a08e9f6ccbc73
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-03-06 01:07:29 -0500

    Attempt 1 at fixing reload

--- commands/ReloadCommand.js
+++ commands/ReloadCommand.js
@@ -9,9 +9,9 @@ module.exports = {
     const commandName = args[0].toLowerCase()
     const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
     if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`!`)
-    delete require.cache[require.resolve(`./${command.name}Command.js`)]
+    delete require.cache[require.resolve(`./${command.properName}Command.js`)]
     try {
-      const newCommand = require(`./${command.name}Command.js`)
+      const newCommand = require(`./${command.properName}Command.js`)
       message.client.commands.set(newCommand.name, newCommand)
     } catch (error) {
       console.error(error)
