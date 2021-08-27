61a4134deec77b937075efecd2cfbe9481916b6e
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-08-27 00:06:03 -0400

    Add avatar command

---
+++ commands/avatar.ts
@@ -0,0 +1,24 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+
+export = {
+  name: 'avatar',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const embed = new MessageEmbed({
+      title: 'Avatar'
+    })
+    if (i.options.getUser('user')) {
+      const target = i.options.getUser('user')
+      embed.setAuthor(target.tag, target.displayAvatarURL({ dynamic: true }))
+      embed.setImage(target.displayAvatarURL({ dynamic: true }))
+      const targetMember = await i.guild.members.fetch(target.id)
+      if (targetMember.displayColor) embed.setColor(targetMember.displayColor)
+    } else {
+      embed.setAuthor(i.user.tag, i.user.displayAvatarURL({ dynamic: true }))
+      embed.setImage(i.user.displayAvatarURL({ dynamic: true }))
+      const selfMember = await i.guild.members.fetch(i.user.id)
+      if (selfMember.displayColor) embed.setColor(selfMember.displayColor)
+    }
+    await i.reply({ embeds: [embed] })
+  }
+}
