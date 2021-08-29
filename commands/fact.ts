d45c5829ad58b66c10ab785f80f840f9222f74de
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-08-29 02:10:01 -0400

    Create fact command

---
+++ commands/fact.ts
@@ -0,0 +1,16 @@
+import { CommandInteraction } from 'discord.js'
+import axios from 'axios'
+
+export = {
+  name: 'fact',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const fact = await axios('https://nekos.life/api/v2/fact').catch(e => console.error(e))
+    if (!fact) {
+      await i.reply({ content: 'The server decided no knowledge for you - please try again later.' })
+      return
+    }
+
+    await i.reply({ content: fact.data.fact })
+  }
+}
