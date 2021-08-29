c982edc12e91ac7a9fc8217fc365f44c0d04c4e3
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-08-29 01:39:24 -0400

    Create birb command

---
+++ commands/birb.ts
@@ -0,0 +1,17 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+import axios from 'axios'
+
+export = {
+  name: 'birb',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const imglist = await axios('https://random.birb.pw/img/')
+    const images = imglist.data.match(/\/img\/\S[^.<]*\.[A-z]*/g)
+    const index = Math.round(Math.random() * (images.length - 1))
+    const embed = new MessageEmbed({ title: 'Tweet Tweet...' })
+    const member = await i.guild.members.fetch(i.user.id)
+    if (member.displayColor) embed.setColor(member.displayColor)
+    embed.setImage(`https://random.birb.pw${images[index]}`)
+    await i.reply({ embeds: [embed] })
+  }
+}
