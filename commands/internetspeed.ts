e658aee2404af9d1455678ed810d3345cb5a9ffd
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-01 21:57:11 -0400

    Create internetspeed command

---
+++ commands/internetspeed.ts
@@ -0,0 +1,14 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+
+export = {
+  name: 'internetspeed',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const embed = new MessageEmbed()
+      .setImage('https://thumbsnap.com/sc/3N5uU9CP.png')
+
+    const member = await i.guild.members.fetch(i.user.id).catch(e => console.error(e))
+    if (member) embed.setColor(member.displayColor)
+    await i.reply({ embeds: [embed] })
+  }
+}
