db05392f0dbc42b7d1eb5af1f0cbb3cdf9a6e6e0
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-01 21:12:01 -0400

    Create dog command

---
+++ commands/dog.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+import axios from 'axios'
+
+export = {
+  name: 'dog',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    try {
+      const dog = await axios('https://dog.ceo/api/breeds/image/random')
+      const embed = new MessageEmbed()
+        .setTitle(':dog: Woof!')
+        .setImage(dog.data.message)
+
+      const member = await i.guild.members.fetch(i.user.id).catch(e => console.error(e))
+      if (member && member.displayColor) embed.setColor(member.displayColor)
+      await i.reply({ embeds: [embed] })
+    } catch (e) {
+      console.error(e)
+      await i.reply({ content: 'The dog giver is on break, please try again later. (HTTP Error)', ephemeral: true })
+    }
+  }
+}
