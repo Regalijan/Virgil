b0ef4da8004b08f6c4a36ac34e304e3082ca02ab
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-01 23:07:08 -0400

    Create owoify command

---
+++ commands/owoify.ts
@@ -0,0 +1,17 @@
+import { CommandInteraction } from 'discord.js'
+
+export = {
+  name: 'owoify',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const text = i.options.getString('text')
+      .replace(/[lr]/g, 'w')
+      .replace(/[LR]/g, 'W')
+      .replace(/n([aeiou])/g, 'ny$1')
+      .replace(/N([aeiou])/g, 'Ny$1')
+      .replace(/N([AEIOU])/g, 'Ny$1')
+      .replace(/ove/g, 'uv')
+
+    await i.reply({ content: text })
+  }
+}
