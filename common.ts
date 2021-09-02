310716da65114a1b03a01f7e23edd9a01f66e23e
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-02 11:50:44 -0400

    Create common.ts

---
+++ common.ts
@@ -0,0 +1,127 @@
+import axios from 'axios'
+import redis from './redis'
+
+export = {
+  async getRobloxMemberGroups (user: number): Promise<{ group: { id: number, name: string, memberCount: number }, role : { id: number, name: string, rank: number } }[]> {
+    const cachedData = await redis.get(`robloxgroups_${user}`).catch(e => console.error(e))
+    if (cachedData) return JSON.parse(cachedData)
+    try {
+      const apiResponse = await axios(`https://groups.roblox.com/v2/users/${user}/groups/roles`)
+      await redis.set(`robloxgroups_${user}`, JSON.stringify(apiResponse.data), 'EX', 900).catch(e => console.error(e))
+      return apiResponse.data
+    } catch (e) {
+      console.error(e)
+      return []
+    }
+  },
+
+  async getRobloxUserFriends (user: number): Promise<number[]> {
+    const cachedData = await redis.get(`robloxfriends_${user}`).catch(e => console.error(e))
+    if (cachedData) return JSON.parse(cachedData)
+    try {
+      const apiResponse = await axios(`https://friends.roblox.com/v1/users/${user}/friends`)
+      const friendIds: number[] = []
+      for (const friend of apiResponse.data.data) friendIds.push(friend.id)
+      await redis.set(`robloxfriends_${user}`, JSON.stringify(friendIds), 'EX', 1800).catch(e => console.error(e))
+      return friendIds
+    } catch (e) {
+      console.error(e)
+      return []
+    }
+  },
+
+  async getRobloxAssetOwnership (user: number, item: number, itemType: 'Asset' | 'Badge' | 'Bundle' | 'GamePass' = 'Badge'): Promise<boolean> {
+    // Accepted values are "Asset", "Badge", "Bundle", and "GamePass"
+    const cachedData = await redis.get(`${itemType}_${item}_${user}`).catch(e => console.error(e))
+    if (cachedData) return JSON.parse(cachedData)
+    try {
+      const apiResponse = await axios(`https://inventory.roblox.com/v1/users/${user}/items/${itemType}/${itemType}`)
+      const ownsItem = apiResponse.data.data.length ? true : false
+      await redis.set(`${itemType}_${item}_${user}`, JSON.stringify(ownsItem), 'EX', 900).catch(e => console.error(e))
+      return ownsItem
+    } catch (e) {
+      console.error(e)
+      return false
+    }
+  },
+
+  async getRobloxPlatformBadges (user: number): Promise<number[]> {
+    /*
+    Roblox platform badge id list, see https://www.roblox.com/info/roblox-badges
+    ----------------------------------------------------------------------------
+    1: Administrator
+    2: Friendship
+    3: Combat Initiation *Deprecated
+    4: Warrior *Deprecated
+    5: Bloxxer *Deprecated
+    6: Homestead
+    7: Bricksmith
+    8: Inviter *Deprecated
+    9: ? *Deleted
+    10: ? *Deleted
+    11: Builders Club *Deleted
+    12: Veteran
+    13: ? *Deleted
+    14: Ambassador *Deprecated
+    15: Turbo Builders Club *Deleted
+    16: Outrageous Builders Club *Deleted
+    17: Official Model Maker
+    18: Welcome To The Club *Deprecated
+    (Unknown badge ids are forum, image, and super moderator)
+    */
+   const cachedData = await redis.get(`robloxplatformbadges_${user}`).catch(e => console.error(e))
+   if (cachedData) return JSON.parse(cachedData)
+   try {
+     const apiResponse = await axios(`https://accountinformation.roblox.com/v1/users/${user}/roblox-badges`)
+     const badgeIds: number[] = []
+     for (const badge of apiResponse.data) badgeIds.push(badge.id)
+     await redis.set(`robloxplatformbadges_${user}`, JSON.stringify(badgeIds), 'EX', 900)
+     return badgeIds
+   } catch (e) {
+     console.error(e)
+     return []
+   }
+  },
+
+  async getRobloxUserProfile (user: number): Promise<{ description: string, created: Date, isBanned: boolean, externalAppDisplayName: string, id: number, name: string, displayName: string } | void> {
+    const cachedData = await redis.get(`robloxprofile_${user}`).catch(e => console.error(e))
+    if (cachedData) return JSON.parse(cachedData)
+    try {
+      const apiResponse = await axios(`https://users.roblox.com/v1/users/${user}`)
+      await redis.set(`robloxprofile_${user}`, JSON.stringify(apiResponse.data), 'EX', 900).catch(e => console.error(e))
+      return apiResponse.data
+    } catch (e) {
+      console.error(e)
+      return
+    }
+  },
+
+  async getRobloxGroupAffiliates (group: number, relationship: 'allies' | 'enemies' = 'allies'): Promise<number[]> {
+    if (!['allies', 'enemies'].includes(relationship.toLowerCase())) throw SyntaxError('relationship must be a value of "allies" or "enemies"')
+    const cachedData = await redis.get(`${relationship}_${group}`).catch(e => console.error(e))
+    if (cachedData) return JSON.parse(cachedData)
+    try {
+      const apiResponse = await axios(`https://groups.roblox.com/v1/groups/${group}/relationships/${relationship}?model.startRowIndex=0&model.maxRows=100`)
+      const groups: number [] = []
+      for (const group of apiResponse.data.relatedGroups) groups.push(group.id)
+      await redis.set(`${relationship}_${group}`, groups, 'EX', 3600).catch(e => console.error(e))
+      return groups
+    } catch (e) {
+      console.error(e)
+      return []
+    }
+  },
+
+  formatName (template: string, discordUsername: string, discordId: string, serverName: string, robloxUsername: string, robloxId: number, displayName: string): string {
+    const name = template
+      .replaceAll('{{USERNAME}}', robloxUsername)
+      .replaceAll('{{DISPLAYNAME}}', displayName)
+      .replaceAll('{{ROBLOXID}}', robloxId.toString())
+      .replaceAll('{{DISCORDNAME}}', discordUsername)
+      .replaceAll('{{DISCORDID}}', discordId)
+      .replaceAll('{{SERVER}}', serverName)
+      .replaceAll('{{SMARTNAME}}', robloxUsername === displayName ? robloxUsername : `${displayName} (${robloxUsername})`)
+    
+    return name.length > 32 ? name.substr(0, 32) : name
+  }
+}
