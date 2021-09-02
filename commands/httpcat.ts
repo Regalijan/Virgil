65ddc155826ee0c3f3818ea390fe33a8ff9b7134
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-01 21:27:26 -0400

    Create httpcat command

---
+++ commands/httpcat.ts
@@ -0,0 +1,15 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+
+export = {
+  name: 'httpcat',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const embed = new MessageEmbed()
+      .setTitle(':cat: Meow!')
+      .setImage(`https://http.cat/${i.options.getInteger('status')}`)
+
+    const member = await i.guild.members.fetch(i.user.id).catch(e => console.error(e))
+    if (member && member.displayColor) embed.setColor(member.displayColor)
+    await i.reply({ embeds: [embed] })
+  }
+}
