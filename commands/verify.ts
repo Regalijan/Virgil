f86f3a18098954fe36ed3bf81435a1cea57e9c51
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-02 23:56:07 -0400

    Create verify command

---
+++ commands/verify.ts
@@ -0,0 +1,12 @@
+import { CommandInteraction } from 'discord.js'
+import Common from '../common'
+
+export = {
+  name: 'verify',
+  permissions: [],
+  async exec (i: CommandInteraction): Promise<void> {
+    const member = await i.guild?.members.fetch(i.user.id)
+    if (!member) return await i.reply({ content: 'An error occured when attempting to verify you - please try again later.', ephemeral: true })
+    await i.reply({ content: await Common.verify(member) })
+  }
+}
