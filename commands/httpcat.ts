a8a2713ca0a9743992f6176e14842a5754f6cb1d
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-07 18:56:39 -0400

    Handle guild possibly being null

--- commands/httpcat.ts
+++ commands/httpcat.ts
@@ -8,7 +8,7 @@ export = {
       .setTitle(':cat: Meow!')
       .setImage(`https://http.cat/${i.options.getInteger('status')}`)
 
-    const member = await i.guild.members.fetch(i.user.id).catch(e => console.error(e))
+    const member = await i.guild?.members.fetch(i.user.id).catch(e => console.error(e))
     if (member && member.displayColor) embed.setColor(member.displayColor)
     await i.reply({ embeds: [embed] })
   }
