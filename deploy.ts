14220f76cdff4a228b8ea4be19ae503b33d55308
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-08 00:42:48 -0400

    Create deploy script

---
+++ deploy.ts
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import { config as dotenv } from 'dotenv'
+import { readdir } from 'fs/promises'
+import { join } from 'path'
+import { ApplicationCommandData } from 'discord.js'
+
+dotenv()
+
+if (!process.env.DISCORDTOKEN) throw Error('No token found in environment!')
+
+const commands: ApplicationCommandData[] = []
+
+;(async function (): Promise<void> {
+  const apiSelf = await axios('https://discord.com/api/v9/users/@me', {
+    headers: {
+      authorization: `Bot ${process.env.DISCORDTOKEN}`
+    }
+  })
+  const uid = apiSelf.data.id
+  for (const file of await readdir(join(__dirname, 'commands'))) {
+    const cFile = require(`./commands/${file}`)
+    commands.push(cFile.interactionData)
+  }
+  const registerResponse = await axios(`https://discord.com/api/v9/applications/${uid}/commands`, {
+    headers: {
+      authorization: `Bot ${process.env.DISCORDTOKEN}`,
+      'content-type': 'application/json'
+    },
+    method: 'PUT',
+    data: JSON.stringify(commands)
+  })
+  console.log(registerResponse.data)
+}())
