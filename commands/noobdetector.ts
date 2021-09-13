3d263c9698faa6e916f53ff5d4aabcc9bcca8030
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-12 22:38:41 -0400

    Be more specific about noob level

--- commands/noobdetector.ts
+++ commands/noobdetector.ts
@@ -20,7 +20,7 @@ export = {
     const member = await i.guild?.members.fetch(target.id).catch(e => console.error(e))
     if (!member) return await i.reply({ content: 'Oops! I could not find that member in the server.', ephemeral: true })
     const embed = new MessageEmbed({ title: 'Noob Detector', author: { name: member.user.tag, icon_url: member.user.displayAvatarURL({ dynamic: true }) }, color: member.displayColor })
-    embed.addField('Noob Level', `${Math.round(Math.random() * 100)}`)
+    embed.addField('Noob Level', `${Math.round(Math.random() * 100)} out of 100`)
     await i.reply({ embeds: [embed] })
   }
 }
