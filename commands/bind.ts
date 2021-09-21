c376ea9d165a8f7566a23b1982233e4e5766137b
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-20 22:34:06 -0400

    Finish basic bind types

--- commands/bind.ts
+++ commands/bind.ts
@@ -1,4 +1,7 @@
 import { CommandInteraction } from 'discord.js'
+import axios from 'axios'
+import { createHash, randomBytes } from 'crypto'
+import mongo from '../mongo'
 
 export = {
   name: 'bind',
@@ -29,6 +32,12 @@ export = {
                     required: true
                   },
                   {
+                    type: 8,
+                    name: 'role',
+                    description: 'The discord role to bind',
+                    required: true
+                  },
+                  {
                     type: 4,
                     name: 'rank',
                     description: 'Optional rank of group role'
@@ -45,6 +54,12 @@ export = {
                     name: 'badge_id',
                     description: 'ID of experience badge',
                     required: true
+                  },
+                  {
+                    type: 8,
+                    name: 'role',
+                    description: 'Discord role to bind',
+                    required: true
                   }
                 ]
               },
@@ -57,6 +72,12 @@ export = {
                     type: 4,
                     name: 'bundle_id',
                     description: 'ID of bundle',
+                    required: true
+                  },
+                  {
+                    type: 8,
+                    name: 'role',
+                    description: 'Discord role to bind',
                     required: true
                   }
                 ]
@@ -71,6 +92,11 @@ export = {
                     name: 'gamepass_id',
                     description: 'ID of gamepass',
                     required: true
+                  },
+                  {
+                    type: 8,
+                    name: 'role',
+                    description: 'Discord role to bind'
                   }
                 ]
               },
@@ -83,6 +109,12 @@ export = {
                     type: 4,
                     name: 'asset_id',
                     description: 'ID of asset',
+                    required: true
+                  },
+                  {
+                    type: 8,
+                    name: 'role',
+                    description: 'Discord role to bind',
                     required: true
                   }
                 ]
@@ -138,10 +170,76 @@ export = {
     ]
   },
   async exec (i: CommandInteraction): Promise<void> {
+    if (!i.guild) throw Error('<CommandInteraction>.guild is null')
+    const subG = i.options.getSubcommandGroup(false)
     const subc = i.options.getSubcommand(true)
-    switch (subc) {
-      case 'group':
-        
+    const bindDb = mongo.db().collection('binds')
+    if (subG === 'create') {
+      const bindId = createHash('sha256').update(randomBytes(256)).digest('base64')
+      switch (subc) {
+        case 'group':
+          if (i.options.getInteger('group_id', true) < 1) return await i.reply({ content: 'Group IDs cannot be negative!', ephemeral: true })
+          if (i.options.getInteger('group_id', true) === 0) return await i.reply({ content: 'You cannot use group 0, please bind another verified role.', ephemeral: true })
+          const groupRequest = await axios('https://groups.roblox.com/v2/groups?groupIds=' + i.options.getInteger('group_id', true)).catch(e => console.error(e))
+          if (!groupRequest) return await i.reply({ content: 'The group could not be validated!', ephemeral: true })
+          if (!groupRequest.data.data?.length) return await i.reply({ content: 'This group does not exist!', ephemeral: true })
+          await bindDb.insertOne({ server: i.guildId, type: 'group', role: i.options.getRole('role', true).id, group: i.options.getInteger('group_id', true), rank: i.options.getInteger('rank') })
+          break
+
+        case 'badge':
+          if (i.options.getInteger('badge_id', true) < 0) return await i.reply({ content: 'Badge IDs cannot be negative!', ephemeral: true })
+          const badgeVerify = await axios(`https://badges.roblox.com/v1/badges/${i.options.getInteger('badge_id')}`).catch(() => {})
+          if (!badgeVerify) return await i.reply({ content: 'Badge could not be validated! Does it exist?', ephemeral: true })
+          await bindDb.insertOne({ server: i.guildId, type: 'badge', role: i.options.getRole('role', true).id, asset: i.options.getInteger('badge_id', true) })
+          break
+
+        case 'bundle':
+          if (i.options.getInteger('bundle_id', true) < 1) return await i.reply({ content: 'Bundle IDs cannot be less than 1!' })
+          const bundleVerify = await axios(`https://catalog.roblox.com/v1/bundles/${i.options.getInteger('bundle_id', true)}/details`, {
+            validateStatus: ((s) => {
+              if (s === 400) return false
+              return true
+            })
+          }).catch(e => console.error(e))
+          if (!bundleVerify) return await i.reply({ content: 'An error occured when looking up the bundle! Please try again later.', ephemeral: true })
+          if (bundleVerify.status === 400) return await i.reply({ content: 'The bundle you specified does not exist.', ephemeral: true })
+          await bindDb.insertOne({ server: i.guildId, type: 'bundle', role: i.options.getRole('role', true), asset: i.options.getInteger('bundle_id', true) })
+          break
+
+        case 'gamepass':
+          if (i.options.getInteger('gamepass_id', true) < 1) return await i.reply({ content: 'GamePass IDs cannot be less than 1!', ephemeral: true })
+          const gamePassVerify = await axios(`https://api.roblox.com/marketplace/game-pass-product-info?gamePassId=${i.options.getInteger('gamepass_id', true)}`, {
+            validateStatus: (s) => {
+              if (s === 400) return false
+              return true
+            }
+          }).catch(e => console.error(e))
+          if (!gamePassVerify) return await i.reply({ content: 'An error occured when looking up that GamePass! Please try again later.' })
+          if (gamePassVerify.status === 400) return await i.reply({ content: 'GamePass does not exist! Try again.', ephemeral: true })
+          await bindDb.insertOne({ server: i.guildId, type: 'gamepass', role: i.options.getRole('role', true), asset: i.options.getInteger('gamepass_id', true) })
+          break
+
+        case 'asset':
+          if (i.options.getInteger('asset_id', true) < 1) return await i.reply({ content: 'Asset IDs cannot be less than 1!', ephemeral: true })
+          const assetVerify = await axios(`https://api.roblox.com/marketplace/productinfo?assetId=${i.options.getInteger('asset_id', true)}`, {
+            validateStatus: (s) => {
+              if (s === 400) return false
+              return true
+            }
+          }).catch(e => console.error(e))
+          if (!assetVerify) return await i.reply({ content: 'An error occured when looking up that asset! Please try again later.', ephemeral: true })
+          if (assetVerify.status === 400) return await i.reply({ content: 'This asset does not exist! Try again.', ephemeral: true })
+          await bindDb.insertOne({ server: i.guildId, type: 'asset', role: i.options.getRole('role', true).id, asset: i.options.getInteger('asset_id', true) })
+          break
+
+        case 'verified_status':
+          await bindDb.insertOne({ server: i.guildId, type: 'verified', role: i.options.getRole('role', true) })
+          break
+
+        case 'unverified_status':
+          await bindDb.insertOne({ server: i.guildId, type: 'unverified', role: i.options.getRole('role', true).id })
+          break
+      }
     }
   }
 }
