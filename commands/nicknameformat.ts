3163f1266e3d22f0a655b19392c9c7976ca6f592
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-21 17:50:19 -0400

    Create nicknameformat command

---
+++ commands/nicknameformat.ts
@@ -0,0 +1,24 @@
+import { CommandInteraction } from 'discord.js'
+import mongo from '../mongo'
+
+const settingsDB = mongo.db().collection('settings')
+
+export = {
+  name: 'nicknameformat',
+  permissions: ['MANAGE_GUILD'],
+  interactionData: {
+    name: 'nicknameformat',
+    description: 'Set format of nicknames to',
+    options: [
+      {
+        type: 3,
+        name: 'format',
+        description: 'The format to use when nicknaming a user - Default {{SMARTNAME}}',
+        required: true
+      }
+    ]
+  },
+  async exec (i: CommandInteraction): Promise<void> {
+    settingsDB.findOneAndUpdate({ server: i.guildId }, { nicknameformat: i.options.get('format', true) })
+  }
+}
