b4767d9aebb131f98a7b6ee3189ac2ca14a02a0f
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-21 20:38:40 -0400

    Create factory reset command

---
+++ commands/factoryreset.ts
@@ -0,0 +1,28 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+import mongo from '../mongo'
+
+export = {
+  name: 'factoryreset',
+  description: 'Erases all settings of the bot and leaves the server',
+  permissions: ['ADMINISTRATOR'],
+  interactionData: {
+    name: 'factoryreset',
+    description: 'Erases all settings of the bot and leaves the server',
+  },
+  async exec (i: CommandInteraction): Promise<void> {
+    const serversettings = await mongo.db().collection('settings').findOneAndDelete({ server: i.guildId })
+    await mongo.db().collection('binds').deleteMany({ server: i.guildId })
+    await i.reply({ content: 'Settings deleted! Leaving the server...', ephemeral: true })
+    const logChannel = await i.guild?.channels.fetch(serversettings.value?.commandLogChannel).catch(e => console.error(e))
+    const me = await i.guild?.me?.fetch()
+    if (logChannel && !me?.permissionsIn(logChannel).has('SEND_MESSAGES') && logChannel.type === 'GUILD_TEXT') {
+      const logEmbed = new MessageEmbed()
+        .setAuthor(i.user.tag, i.user.displayAvatarURL({ dynamic: true }))
+        .setColor(serversettings.value?.commandLogChannel ?? 3756250)
+        .setDescription('Requested factory reset.')
+
+      await logChannel.send({ embeds: [logEmbed] })
+    }
+    await i.guild?.leave()
+  }
+}
