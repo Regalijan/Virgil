fc9c978830e1f9f559513c226a34b54e4870190c
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-22 00:50:54 -0400

    Add asset id to listbinds embed

--- commands/listbinds.ts
+++ commands/listbinds.ts
@@ -18,7 +18,8 @@ export = {
     if (i.member instanceof GuildMember) embed.setColor(i.member.displayColor)
     for (const bind of bindsList) {
       let bindString = bind.type[0].toUpperCase() + bind.type.slice(1)
-      if (bind.group) bindString += `${bind.group} - ${bind.rank ? `Rank ${bind.rank}` : 'All ranks' }`
+      if (bind.group) bindString += ` ${bind.group} - ${bind.rank ? `Rank ${bind.rank}` : 'All ranks' }`
+      if (bind.asset) bindString += ` ${bind.asset}`
       embed.addField(bindString, `<@&${bind.role}>`)
     }
     await i.reply({ embeds: [embed] })
