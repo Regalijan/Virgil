db1fe22354afee7c06d5152b8dec16d7d932a87e
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-21 21:43:10 -0400

    Create deletebind command

---
+++ commands/deletebind.ts
@@ -0,0 +1,25 @@
+import { CommandInteraction } from 'discord.js'
+import mongo from '../mongo'
+
+const bindDb = mongo.db().collection('binds')
+
+export = {
+  name: 'deletebind',
+  description: 'Deletes a bind',
+  interactionData: {
+    name: 'deletebind',
+    description: 'Deletes a bind',
+    options: [
+      {
+        type: 3,
+        name: 'id',
+        description: 'ID of the bind to delete',
+        required: true
+      }
+    ]
+  },
+  async exec (i: CommandInteraction): Promise<void> {
+    const bind = await bindDb.findOneAndDelete({ id: i.options.getString('id', true) })
+    await i.reply({ content: bind.value ? 'Bind deleted!' : 'Bind does not exist!', ephemeral: !Boolean(bind.value) })
+  }
+}
