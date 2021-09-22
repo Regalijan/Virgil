41a3b80a5c1e462958668ffb5ab8a6fe55619020
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-21 20:12:14 -0400

    Create whois command

---
+++ commands/whois.ts
@@ -0,0 +1,36 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+
+export = {
+  name: 'whois',
+  permissions: [],
+  interactionData: {
+    name: 'whois',
+    description: 'Get user information',
+    options: [
+      {
+        type: 6,
+        name: 'user',
+        description: 'User to look up'
+      }
+    ]
+  },
+  async exec (i: CommandInteraction): Promise<void> {
+    const user = i.options.getUser('user') ?? i.user
+    const embed = new MessageEmbed({ title: 'User Information' })
+      .setDescription('Profile of ' + user.tag)
+      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+      .addFields(
+        { name: 'Username', value: i.id },
+        { name: 'User ID', value: user.id },
+        { name: 'Joined Discord at', value: user.createdAt.toString() }
+      )
+    if (i.inGuild()) {
+      const member = await i.guild?.members.fetch(user.id)
+      embed.addFields(
+        { name: 'Highest Role', value: `${member?.roles.highest.id}` },
+        { name: 'Joined server at', value: `${member?.joinedAt}` }
+      )
+    }
+    await i.reply({ embeds: [embed] })
+  }
+}
