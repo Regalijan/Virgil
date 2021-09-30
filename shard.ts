fdd8f641cb69681dffb1b4c2fb4074d3abfce272
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-30 00:07:25 -0400

    Add voice tracking

--- shard.ts
+++ shard.ts
@@ -4,14 +4,19 @@ import { unlink, writeFile } from 'fs/promises'
 import { join } from 'path'
 import {
   ApplicationCommandData,
+  CategoryChannel,
   Client,
   CommandInteraction,
   GuildMember,
   Interaction,
   MessageEmbed,
+  NewsChannel,
   PermissionResolvable,
   ShardClientUtil,
-  TextChannel
+  StageChannel,
+  StoreChannel,
+  TextChannel,
+  VoiceChannel
 } from 'discord.js'
 import Sentry from './sentry'
 import db from './mongo'
@@ -318,6 +323,74 @@ bot.on('messageUpdate', async function (oldMessage, newMessage): Promise<void> {
   if (!channel || !(channel instanceof TextChannel) || !newMessage.client.user?.id || !channel.permissionsFor(newMessage.client.user.id)?.has('SEND_MESSAGES')) return
   await channel.send({ embeds: [embed] }).catch(e => {
     process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+})
+
+bot.on('voiceStateUpdate', async function (oldState, newState): Promise<void> {
+  if (!newState.member) return
+  const settings = await mongo.collection('settings').findOne({ guild: newState.guild.id }).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+  if (!settings) return
+  const embed = new MessageEmbed()
+    .setColor(newState.member.displayColor)
+    .setAuthor(newState.member.user.tag, newState.member.user.displayAvatarURL({ dynamic: true }))
+    .setFooter(`ID: ${newState.member.id}`)
+
+  let actionstring = `<@${newState.member.id}> `
+  let logChannel: void | TextChannel | NewsChannel | VoiceChannel | CategoryChannel | StoreChannel | StageChannel | null
+  if (newState.selfMute !== oldState.selfMute && settings.voiceMuteLogChannel) {
+    actionstring += `${newState.mute ? 'muted' : 'unmuted'} themself.`
+    logChannel = await newState.guild.channels.fetch(settings.voiceMuteLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (newState.selfDeaf !== oldState.selfDeaf && settings.voiceDeafenLogChannel) {
+    actionstring += `${newState.deaf ? 'deafened' : 'undeafened'} themself.`
+    logChannel = await newState.guild.channels.fetch(settings.voiceDeafenLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (newState.serverMute !== oldState.serverMute && settings.voiceMuteLogChannel) {
+    actionstring += `was ${newState.serverMute ? 'muted' : 'unmuted'} by a server moderator.`
+    logChannel = await newState.guild.channels.fetch(settings.voiceMuteLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (newState.serverDeaf !== oldState.serverDeaf && settings.voiceDeafenLogChannel) {
+    actionstring += `was ${newState.serverDeaf ? 'deafened' : 'undeafened' }`
+    logChannel = await newState.guild.channels.fetch(settings.voiceDeafenLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (newState.selfVideo !== oldState.selfVideo && settings.voiceVideoLogChannel) {
+    actionstring += `${newState.selfVideo ? 'enabled' : 'disabled'} their video feed.`
+    logChannel = await newState.guild.channels.fetch(settings.voiceVideoLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (newState.channel && !oldState.channel && settings.voiceJoinLogChannel) {
+    actionstring += `joined <#${newState.channelId}>`
+    logChannel = await newState.guild.channels.fetch(settings.voiceJoinLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (!newState.channel && oldState.channel && settings.voiceLeaveLogChannel) {
+    actionstring += `left <#${oldState.channelId}>`
+    logChannel = await newState.guild.channels.fetch(settings.voiceLeaveLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  else if (newState.channel && oldState.channel && settings.voiceSwitchLogChannel && newState.channelId !== oldState.channelId) {
+    actionstring += `switched from <#${oldState.channelId}> to <#${newState.channelId}>`
+    logChannel = await newState.guild.channels.fetch(settings.voiceSwitchLogChannel).catch(e => {
+      process.env.DSN ? Sentry.captureException(e) : console.error(e)
+    })
+  }
+  if (actionstring === `<@${newState.member.id}> ` || logChannel?.type !== 'GUILD_TEXT' || !newState.client.user || !logChannel?.permissionsFor(newState.client.user.id)?.has('SEND_MESSAGES')) return
+  embed.setDescription(actionstring)
+  await logChannel.send({ embeds: [embed] }).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.log(e)
   })
 })
 
