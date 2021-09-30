8aa3a4c23b6849007d1a9cc1f36e79a9375bfd92
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-09-29 22:50:57 -0400

    Apparently there is a json endpoint for birb api

--- commands/birb.ts
+++ commands/birb.ts
@@ -9,13 +9,11 @@ export = {
     description: 'Gets picture of birb'
   },
   async exec (i: CommandInteraction): Promise<void> {
-    const imglist = await axios('https://random.birb.pw/img/')
-    const images = imglist.data.match(/\/img\/\S[^.<]*\.[A-z]*/g)
-    const index = Math.round(Math.random() * (images.length - 1))
     const embed = new MessageEmbed({ title: 'Tweet Tweet...' })
     const member = await i.guild?.members.fetch(i.user.id).catch(e => console.error(e))
     if (member?.displayColor) embed.setColor(member.displayColor)
-    embed.setImage(`https://random.birb.pw${images[index]}`)
+    const birbRequest = await axios('https://random.birb.pw/tweet.json')
+    embed.setImage(`https://random.birb.pw/img/${birbRequest.data.file}`)
     await i.reply({ embeds: [embed] })
   }
 }
