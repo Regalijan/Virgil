bb0c95e5772b8879ac2b497f4ccdbf0c9915f435
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-06 20:48:50 -0400

    Create troubleshoot command

---
+++ commands/troubleshoot.ts
@@ -0,0 +1,19 @@
+import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js'
+
+export = {
+  name: 'troubleshoot',
+  permissions: [],
+  interactionData : {
+    name: 'troubleshoot',
+    description: 'Troubleshoot Roblox verification connection issues.'
+  },
+  async exec (i: CommandInteraction): Promise<void> {
+    const button = new MessageButton()
+      .setURL(`https://rover.link/validate/${Buffer.from(`${i.user.id};${i.user.username}#${i.user.tag}`).toString('base64')}`)
+      .setEmoji('ℹ️')
+      .setLabel('Open Troubleshooting Page')
+      .setStyle('LINK')
+    
+    await i.reply({ content: 'Click the button to continue.', ephemeral: true, components: [new MessageActionRow({ components: [button] })] })
+  }
+}
