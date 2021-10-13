d4d3b10e46a895a2cfb523a07175424956f8af74
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-13 17:07:34 -0400

    Create threadCreate listener

--- shard.ts
+++ shard.ts
@@ -331,6 +331,25 @@ bot.on('messageUpdate', async function (oldMessage, newMessage): Promise<void> {
   })
 })
 
+bot.on('threadCreate', async function (thread): Promise<void> {
+  const settings = await mongo.collection('settings').findOne({ guild: thread.guildId }).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+  if (!settings?.threadCreateLogChannel) return
+  const embed = new MessageEmbed()
+    .setDescription(`Thread <#${thread.id}> created by <@${thread.ownerId}>`)
+    .setFooter(`Thread ${thread.id}`)
+    .setColor([0,255,0])
+  
+  const channel = await thread.guild.channels.fetch(settings.threadCreateLogChannel).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+  if (!channel || channel.type !== 'GUILD_TEXT' || !thread.guild.me?.permissionsIn(channel).has('SEND_MESSAGES')) return
+  await channel.send({ embeds: [embed] }).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+})
+
 bot.on('voiceStateUpdate', async function (oldState, newState): Promise<void> {
   if (!newState.member) return
   const settings = await mongo.collection('settings').findOne({ guild: newState.guild.id }).catch(e => {
