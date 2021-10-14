2d0a568c2775f2a727902edfb786f5a1df668520
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-13 23:57:44 -0400

    Add thread delete log

--- shard.ts
+++ shard.ts
@@ -387,6 +387,25 @@ bot.on('threadCreate', async function (thread): Promise<void> {
   })
 })
 
+bot.on('threadDelete', async function (thread): Promise<void> {
+  const settings = await mongo.collection('settings').findOne({ guild: thread.guildId }).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+  if (!settings?.threadDeleteLogChannel) return
+  const embed = new MessageEmbed()
+    .setDescription(`Thread ${thread.name} deleted.`)
+    .setFooter(`Thread ${thread.id}`)
+    .setColor([255,0,0])
+
+  const channel = await thread.guild.channels.fetch(settings.threadDeleteLogChannel).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+  if (!channel || channel.type !== 'GUILD_TEXT' || !thread.guild.me?.permissionsIn(channel).has('SEND_MESSAGES')) return
+  await channel.send({ embeds: [embed] }).catch(e => {
+    process.env.DSN ? Sentry.captureException(e) : console.error(e)
+  })
+})
+
 bot.on('voiceStateUpdate', async function (oldState, newState): Promise<void> {
   if (!newState.member) return
   const settings = await mongo.collection('settings').findOne({ guild: newState.guild.id }).catch(e => {
