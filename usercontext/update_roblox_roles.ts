c430464fd85162a8175c56862ca69b35b172789a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-14 13:05:43 -0400

    Change file name

--- usercontext/update_roblox_roles.ts
+++ usercontext/update_roblox_roles.ts
@@ -2,17 +2,17 @@ import { ContextMenuInteraction } from 'discord.js'
 import Common from '../common'
 
 export = {
-  name: 'context_update',
+  name: 'Update Roblox Roles',
   permissions: [],
   interactionData: {
-    name: 'context_update',
+    name: 'Update Roblox Roles',
     type: 2
   },
 
   async exec (i: ContextMenuInteraction): Promise<void> {
     if (i.targetType !== 'USER') return await i.reply({ content: 'User context expected but given message.', ephemeral: true })
-    const member = await i.guild?.members.fetch(i.user.id)
-    if (!member) return await i.reply({ content: '`Error: <ContextMenuInteraction>.guild is null`', ephemeral: true })
+    const member = await i.guild?.members.fetch(i.targetId)
+    if (!member) return await i.reply({ content: '`Error: <ContextMenuInteraction>.guild is null or member does not exist.`', ephemeral: true })
     if (i.targetId !== i.user.id && !member.permissions.has('MANAGE_GUILD')) return await i.reply({ content: 'Oops! You do not have permission to use this command; if you want to verify yourself, please run `/verify`', ephemeral: true })
     await i.reply({ content: await Common.verify(member, i.targetId === i.user.id), ephemeral: true })
   }
