0b06148f7996e86ffa612b04669df658ef755f38
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-14 13:42:58 -0400

    Fix roblox update user context command

--- usercontext/update_roblox_roles.ts
+++ usercontext/update_roblox_roles.ts
@@ -13,7 +13,7 @@ export = {
     if (i.targetType !== 'USER') return await i.reply({ content: 'User context expected but given message.', ephemeral: true })
     const member = await i.guild?.members.fetch(i.targetId)
     if (!member) return await i.reply({ content: '`Error: <ContextMenuInteraction>.guild is null or member does not exist.`', ephemeral: true })
-    if (i.targetId !== i.user.id && !member.permissions.has('MANAGE_GUILD')) return await i.reply({ content: 'Oops! You do not have permission to use this command; if you want to verify yourself, please run `/verify`', ephemeral: true })
+    if (i.targetId !== i.user.id && !i.memberPermissions?.has('MANAGE_GUILD')) return await i.reply({ content: 'Oops! You do not have permission to use this command; if you want to verify yourself, please run `/verify`', ephemeral: true })
     await i.reply({ content: await Common.verify(member, i.targetId === i.user.id), ephemeral: true })
   }
 }
