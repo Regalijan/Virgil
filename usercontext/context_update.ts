c20b51ac0878cc84ab0777aac323616f89e1bef6
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-13 23:42:58 -0400

    Create update user context command

---
+++ usercontext/context_update.ts
@@ -0,0 +1,20 @@
+import { ContextMenuInteraction } from 'discord.js'
+import Common from '../common'
+
+export = {
+  name: 'context_update',
+  permissions: [],
+  interactionData: {
+    name: 'context_update',
+    type: 2,
+    description: 'Context command for updating user'
+  },
+
+  async exec (i: ContextMenuInteraction): Promise<void> {
+    if (i.targetType !== 'USER') return await i.reply({ content: 'User context expected but given message.', ephemeral: true })
+    const member = await i.guild?.members.fetch(i.user.id)
+    if (!member) return await i.reply({ content: '`Error: <ContextMenuInteraction>.guild is null`', ephemeral: true })
+    if (i.targetId !== i.user.id && !member.permissions.has('MANAGE_GUILD')) return await i.reply({ content: 'Oops! You do not have permission to use this command; if you want to verify yourself, please run `/verify`', ephemeral: true })
+    await i.reply({ content: await Common.verify(member, i.targetId === i.user.id), ephemeral: true })
+  }
+}
