ea4ef9da9df876260d9a3bd2b5d54e275d5286d5
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-21 00:24:26 -0400

    Create base message report ignore button command

---
+++ button/msg_report_ignore.ts
@@ -0,0 +1,33 @@
+import { ButtonInteraction } from "discord.js";
+import mongo from "../mongo";
+const reportStore = mongo.db("bot").collection("reports");
+
+export = {
+  name: "msg_report_ignore",
+  permissions: ["MANAGE_MESSAGES"],
+  async exec(i: ButtonInteraction): Promise<void> {
+    const associatedReport = await reportStore.findOne({
+      reportEmbedId: i.message.id,
+    });
+    if (!associatedReport) {
+      return await i.reply({
+        content: "The report could not be found! Was it already acted upon?",
+        ephemeral: true,
+      });
+    }
+    if (!i.guild?.me?.permissionsIn(i.channelId).has("MANAGE_MESSAGES")) {
+      return await i.reply({
+        content:
+          "I do not have permission to manage messages in this channel! Please allow me to delete messages so reports can marked as resolved.",
+        ephemeral: true,
+      });
+    }
+    await reportStore.deleteOne({
+      reportEmbedId: i.message.id,
+    });
+    await i.reply({
+      content: "Report ignored!",
+      ephemeral: true,
+    });
+  },
+};
