a4173acbd17e55f48214cb5ba9a48fb8c5de26f2
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-11-04 10:29:27 -0400

    Remove object storage part from blacklist command

--- commands/BlacklistCommand.js
+++ commands/BlacklistCommand.js
@@ -12,8 +12,6 @@ module.exports = {
     if (usermodcheck.rowCount === 0 && !message.members.roles.cache.some(role => roles.includes(role.id))) return
     const modsettings = await db.query('SELECT * FROM gamemod_settings WHERE guild = $1;', [message.guild.id])
     if (modsettings.rowCount === 0) return
-    const { Storage } = require('@google-cloud/storage')
-    const storage = new Storage({ keyFilename: `./servicekeys/${message.guild.id}.json` })
     const { Datastore } = require('@google-cloud/datastore')
     const banStore = new Datastore({ keyFilename: './servicekeys/banstore.json' })
     const fs = require('fs/promises')
@@ -26,20 +24,10 @@ module.exports = {
       if (!robloxData.data.Id || robloxData.data.Id === '') return message.channel.send(`I could not find a Roblox user with the name of ${args[0]}`)
       try {
         await banStore.save({ key: banStore.key(['bans']), data: { roblox_id: robloxData.data.Id, banned: false, hidden_from_leaderboards: true, radio_disabled: false, radio_disabled_until: null } })
-        await fs.writeFile(`./${robloxData.data.Id}.json`, '{"usercode":"0x1"}')
       } catch (e) {
-        message.channel.send('An error occured when writing the file!')
-      }
-      try {
-        await storage.bucket(modsettings.rows[0].bucket).upload(`./${robloxData.data.Id}.json`)
-        const viewcheck = await request(`https://storage.googleapis.com/${modsettings.rows[0].bucket}/${robloxData.data.Id}.json`, { validateStatus: false })
-        if (viewcheck.status === 403 && modsettings.rows[0].files_are_public) await storage.bucket(modsettings.rows[0].bucket).file(`${robloxData.data.id}.json`).makePublic()
-      } catch (e) {
-        message.channel.send(`An error occured! ${e}`)
-        return console.error(e)
+        message.channel.send('An error occurred when creating the entity!')
       }
       await message.channel.send(`${robloxData.data.Username} successfully blacklisted!`)
-      await fs.unlink(`./${robloxData.data.Id}.json`, err => { if (err) return console.error(err) })
     } else {
       return message.reply('You did not provide a username!')
     }
