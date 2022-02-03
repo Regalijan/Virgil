220db15a60ea816c9746c3c1968c07e39ef11ad9
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-02-02 20:20:39 -0500

    Create faster sharding system

--- index.ts
+++ index.ts
@@ -1,21 +1,39 @@
 import { config as dotenv } from "dotenv";
 import { ShardingManager } from "discord.js";
 import { join } from "path";
-import mongo from "./mongo";
+import axios from "axios";
 
 dotenv();
 
-mongo.connect();
-
-if (!process.env.DISCORDTOKEN)
-  throw Error("No token was detected in the environment!");
-
-const shardMgr = new ShardingManager(join(__dirname, "shard.js"), {
-  token: process.env.DISCORDTOKEN,
-});
-
-shardMgr.on("shardCreate", function (shard) {
-  console.log(`Launching shard ${shard.id + 1} of ${shardMgr.totalShards}.`);
-});
-
-shardMgr.spawn();
+(async function () {
+  if (!process.env.DISCORDTOKEN)
+    return console.error("No token was detected in the environment!");
+  const gatewayRequest = await axios("https://discord.com/api/v9/gateway/bot", {
+    headers: {
+      authorization: `Bot ${process.env.DISCORDTOKEN}`,
+    },
+  });
+  if (gatewayRequest.status !== 200)
+    throw new Error("Gateway information request failed!");
+  const shardCount = gatewayRequest.data.shards;
+  const shardMgr = new ShardingManager(join(__dirname, "shard.js"), {
+    token: process.env.DISCORDTOKEN,
+    totalShards: shardCount,
+  });
+  shardMgr.on("shardCreate", function (shard) {
+    console.log(`Launching shard ${shard.id + 1} of ${shardMgr.totalShards}.`);
+  });
+  const maxConcurrency =
+    gatewayRequest.data.session_start_limit.max_concurrency;
+  let spawnBucketStart = Date.now();
+  for (let i = 0; i < shardCount; i++) {
+    if (
+      (i + 1) % maxConcurrency === 0 &&
+      Date.now() - 5000 < spawnBucketStart
+    ) {
+      await new Promise((r) => setTimeout(r, Date.now() - spawnBucketStart));
+      spawnBucketStart = Date.now();
+    }
+    await shardMgr.spawn({ amount: 1, delay: 0 });
+  }
+})();
