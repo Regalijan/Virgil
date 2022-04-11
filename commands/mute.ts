7e567b29830f858412eb2626d9bb00f968f6df2a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-04-11 03:39:14 -0400

    Change mute command to timeout functionality

--- commands/mute.ts
+++ commands/mute.ts
@@ -1,7 +1,4 @@
 import { CommandInteraction } from "discord.js";
-import mongo from "../mongo";
-
-const db = mongo.db("bot");
 
 export = {
   name: "mute",
@@ -9,31 +6,30 @@ export = {
   privileged: true,
   async exec(i: CommandInteraction): Promise<void> {
     if (!i.guild) throw TypeError("<CommandInteraction>.guild is null");
-    if (!i.guild.me?.permissions.has("MANAGE_ROLES"))
+    if (!i.guild.me?.permissions.has("MODERATE_MEMBERS"))
       return await i.reply({
         content:
-          'I cannot mute as I do not have the "Manage Roles" permission.',
+          'I cannot mute as I do not have the "Timeout members" permission.',
         ephemeral: true,
       });
-    const settings = await db
-      .collection("settings")
-      .findOne({ guild: i.guild?.id });
-    if (!settings?.muteRole) return;
-    const targetUser = i.options.getUser("user", true);
-    const targetGuildMember = await i.guild.members.fetch(targetUser.id);
-    if (!targetGuildMember.roles.cache.has(settings.muteRole))
+    const targetGuildMember = await i.guild.members.fetch(
+      i.options.getUser("user", true).id
+    );
+    if (targetGuildMember.communicationDisabledUntil)
       return await i.reply({
-        content: "The user already has the mute role.",
+        content: "The user is already muted!",
         ephemeral: true,
       });
-    await db.collection("mutes").insertOne({
-      guild: i.guild.id,
-      member: targetGuildMember.id,
-      expires:
-        Date.now() +
-        (i.options.getInteger("hours") ?? 0) * 360000 +
-        (i.options.getInteger("minutes") ?? 0) * 60000,
+    const minutes = i.options.getNumber("minutes", true);
+    const hours = i.options.getNumber("hours", true);
+    const timeoutLength = minutes * 60 * 1000 + hours * 60 * 60 * 1000;
+    const reason = i.options.getString("reason", false);
+    await targetGuildMember.timeout(
+      timeoutLength,
+      reason || "No reason given."
+    );
+    await i.reply({
+      content: targetGuildMember.user.username + " has been muted.",
     });
-    await i.reply({ content: targetUser.username + " has been muted." });
   },
 };
