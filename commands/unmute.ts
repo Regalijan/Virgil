005508be518cdc4883a86e93894e212271020ead
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-04-11 03:54:03 -0400

    Create unmute command and definition

---
+++ commands/unmute.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, GuildMember } from "discord.js";
+
+export = {
+  name: "unmute",
+  permissions: ["MODERATE_MEMBERS"],
+  privileged: true,
+  async exec(i: CommandInteraction): Promise<void> {
+    if (!i.guild)
+      return await i.reply({
+        content: "This command can only be used in a guild.",
+        ephemeral: true,
+      });
+    if (!i.guild.me?.permissions.has("MODERATE_MEMBERS"))
+      return await i.reply({
+        content:
+          'I cannot unmute the user because I do not have the "Timeout members" permission.',
+        ephemeral: true,
+      });
+    let targetMember = i.options.getMember("user", true);
+
+    if (!(targetMember instanceof GuildMember))
+      targetMember = await i.guild.members.fetch(
+        i.options.getUser("user", true).id
+      );
+    await targetMember.timeout(
+      null,
+      `Timeout removed by ${i.user.tag} (${i.user.id})`
+    );
+  },
+};
