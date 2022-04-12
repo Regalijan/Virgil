864ce25052664c9bb9867d3fc1091c32e215ffee
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-04-12 16:25:04 -0400

    Allow sending components in logs

--- send_log.ts
+++ send_log.ts
@@ -1,4 +1,9 @@
-import { DiscordAPIError, Guild, MessageEmbed } from "discord.js";
+import {
+  DiscordAPIError,
+  Guild,
+  MessageActionRow,
+  MessageEmbed,
+} from "discord.js";
 import mongo from "./mongo";
 
 const settings = mongo.db("bot").collection("settings");
@@ -7,7 +12,8 @@ export default async function (
   url: string,
   embed: MessageEmbed,
   guild: Guild,
-  settingName: string
+  settingName: string,
+  actionRows?: MessageActionRow[]
 ): Promise<void> {
   const webhookIdArr = url.match(/\d{17,19}/);
   if (!webhookIdArr) return;
@@ -29,10 +35,16 @@ export default async function (
     }
     return;
   }
-  await webhookData
-    .send({
-      avatarURL: guild.client.user?.displayAvatarURL(),
-      embeds: [embed.toJSON()],
-    })
-    .catch(console.error);
+  const sendOpts: {
+    avatarURL?: string;
+    components?: MessageActionRow[];
+    embeds: MessageEmbed[];
+  } = {
+    avatarURL: guild.client.user?.displayAvatarURL(),
+    embeds: [embed],
+  };
+
+  if (actionRows) sendOpts.components = actionRows;
+
+  await webhookData.send(sendOpts).catch(console.error);
 }
