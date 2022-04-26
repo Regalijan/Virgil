4460993364b0c2405a54bd3981c570c42f1c82c2
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-04-25 22:20:34 -0400

    Create webhook deleter file

---
+++ webhook_delete.ts
@@ -0,0 +1,15 @@
+import { Guild } from "discord.js";
+
+export default async function (
+  webhookUrl: string,
+  messageId: string,
+  guild: Guild
+): Promise<void> {
+  const webhookParts = webhookUrl
+    .replace(/https:\/\/discord.com\/api\/?v?\d{0,2}?\/webhooks\//, "")
+    .split("/");
+  const webhook = await guild.client
+    .fetchWebhook(webhookParts[0], webhookParts[1])
+    .catch(console.error);
+  await webhook?.delete(messageId).catch(console.error);
+}
