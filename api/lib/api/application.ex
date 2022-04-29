6b9d731ec338e163158b0841be7bbc0532d16720
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-04-29 01:50:42 -0400

    Create base application entry

---
+++ api/lib/api/application.ex
@@ -0,0 +1,39 @@
+defmodule Api.Application do
+  use Application
+
+  Dotenv.load(".env")
+
+  def get_child_opts() do
+    if elem(:os.type(), 1) === :linux &&
+         elem(
+           System.cmd("grep", ["docker-init", "/proc/1/sched"]),
+           0
+         ) !== "" do
+      [port: 0, ip: {:local, "/socket/api.sock"}]
+    else
+      [port: String.to_integer(System.get_env("API_PORT", "8899"))]
+    end
+  end
+
+  @impl true
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
+  def start(_type, _args) do
+    if System.get_env("DISABLE_SERVER") !== nil do
+      System.stop(0)
+    end
+
+    children = [
+      %{
+        id: Mongo,
+        start: {Mongo, :start_link, [[name: :mongo, url: System.get_env("MONGOURL", "mongodb://mongo:27017/bot")]]}
+      },
+      {
+        Plug.Cowboy,
+        scheme: :http, plug: APIRouter, options: get_child_opts()
+      }
+    ]
+    opts = [strategy: :one_for_one, name: Api.Supervisor]
+
+    Supervisor.start_link(children, opts)
+  end
+end
