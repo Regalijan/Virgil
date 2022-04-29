b566b2404b1286b74116d77710b7c7aef001b47d
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-04-29 18:46:14 -0400

    Remove socket on start if in docker

--- api/lib/api/application.ex
+++ api/lib/api/application.ex
@@ -3,12 +3,14 @@ defmodule Api.Application do
 
   Dotenv.load(".env")
 
+  @spec get_child_opts :: [{:ip, {:local, <<_::128>>}} | {:port, integer}, ...]
   def get_child_opts() do
     if elem(:os.type(), 1) === :linux &&
          elem(
            System.cmd("grep", ["docker-init", "/proc/1/sched"]),
            0
          ) !== "" do
+      File.rm("/socket/api.sock")
       [port: 0, ip: {:local, "/socket/api.sock"}]
     else
       [port: String.to_integer(System.get_env("API_PORT", "8899"))]
@@ -25,13 +27,16 @@ defmodule Api.Application do
     children = [
       %{
         id: Mongo,
-        start: {Mongo, :start_link, [[name: :mongo, url: System.get_env("MONGOURL", "mongodb://mongo:27017/bot")]]}
+        start:
+          {Mongo, :start_link,
+           [[name: :mongo, url: System.get_env("MONGOURL", "mongodb://mongo:27017/bot")]]}
       },
       {
         Plug.Cowboy,
         scheme: :http, plug: APIRouter, options: get_child_opts()
       }
     ]
+
     opts = [strategy: :one_for_one, name: Api.Supervisor]
 
     Supervisor.start_link(children, opts)
