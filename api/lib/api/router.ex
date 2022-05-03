0861fdca6662636d025760a825802e4d8d94beed
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-05-03 17:33:05 -0400

    Remove put and patch methods

--- api/lib/api/router.ex
+++ api/lib/api/router.ex
@@ -22,14 +22,6 @@ defmodule APIRouter do
     |> halt
   end
 
-  defp set_guild_id(body_params, gid) do
-    cond do
-      !Map.has_key?(body_params, "guild") -> Map.merge(body_params, %{guild: gid})
-      Map.get(body_params, "guild") !== gid -> Map.replace!(body_params, "guild", gid)
-      true -> body_params
-    end
-  end
-
   get "/guild/:id" do
     doc = Mongo.find_one(:mongo, "settings", %{guild: id})
 
@@ -38,26 +30,6 @@ defmodule APIRouter do
         is_tuple(doc) -> {500, %{error: "Failed to fetch guild settings"}}
         is_nil(doc) -> {404, %{error: "No settings found for guild"}}
         true -> {200, Map.delete(doc, "_id")}
-      end
-
-    respond(conn, status, body)
-  end
-
-  put "/guild/:id" do
-    doc = set_guild_id(conn.body_params, id)
-
-    Mongo.replace_one!(:mongo, "settings", %{guild: id}, doc, upsert: true)
-    respond(conn, 200, %{success: true})
-  end
-
-  patch "/guild/:id" do
-    {s, result} = Mongo.update_one(:mongo, "settings", %{guild: id}, conn.assigns[:upd_doc])
-
-    {status, body} =
-      cond do
-        s !== :ok -> {500, %{error: "Failed to update guild settings"}}
-        result.matched_count > 0 -> {200, %{success: true}}
-        true -> {404, %{error: "No existing settings for guild"}}
       end
 
     respond(conn, status, body)
