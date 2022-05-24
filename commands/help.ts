0131bc2c4106def4052d179a778aaa6bd3d2ef93
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-05-24 18:12:24 -0400

    Trim and remove .git suffix

--- commands/help.ts
+++ commands/help.ts
@@ -4,8 +4,13 @@ import { execSync } from "child_process";
 export = {
   name: "help",
   async exec(i: CommandInteraction): Promise<void> {
-    const remote = execSync("git config --get remote.origin.url").toString();
-    const branch = execSync("git rev-parse --abbrev-ref HEAD").toString();
+    const remote = execSync("git config --get remote.origin.url")
+      .toString()
+      .trim()
+      .replace(/\.git$/, "");
+    const branch = execSync("git rev-parse --abbrev-ref HEAD")
+      .toString()
+      .trim();
     await i.reply({
       content: `Read the manual at ${remote}/blob/${branch}/MANUAL.md`,
     });
