8b38e63636fc8216a21427f9944e01ed96573646
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-06-16 01:12:36 -0400

    Create legal command

---
+++ commands/legal.ts
@@ -0,0 +1,19 @@
+import { CommandInteraction } from "discord.js";
+import { execSync } from "child_process";
+
+export = {
+  name: "legal",
+  async exec(i: CommandInteraction) {
+    const remote = execSync("git config --get remote.origin.url")
+      .toString()
+      .trim()
+      .replace(/\.git$/, "");
+    const branch = execSync("git rev-parse --abbrev-ref HEAD")
+      .toString()
+      .trim();
+    return await i.reply({
+      content: `View the Terms of Service at ${remote}/blob/${branch}/Terms.md\nView the Privacy Policy at ${remote}/blob/${branch}/Privacy.md`,
+      ephemeral: true,
+    });
+  },
+};
