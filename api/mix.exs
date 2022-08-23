a4df35cf70bbe5a8cb1059c8d15e4ca9aecbc711
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-08-23 04:01:07 -0400

    Add Redix

--- api/mix.exs
+++ api/mix.exs
@@ -19,7 +19,8 @@ defmodule Api.MixProject do
         :finch,
         :jason,
         :mongodb_driver,
-        :plug_cowboy
+        :plug_cowboy,
+        :redix
       ],
       extra_applications: [:logger],
       mod: {Api.Application, []}
@@ -33,7 +34,8 @@ defmodule Api.MixProject do
       {:finch, "~> 0.13.0"},
       {:jason, "~> 1.3"},
       {:mongodb_driver, "~> 0.9.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:redix, "~> 1.1"}
     ]
   end
 end
