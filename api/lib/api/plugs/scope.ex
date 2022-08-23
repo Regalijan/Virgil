3275625bae66f8aa8d44aa061c5f48891e63358a
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-08-23 00:42:50 -0400

    Add spec for invalid_scope method

--- api/lib/api/plugs/scope.ex
+++ api/lib/api/plugs/scope.ex
@@ -6,9 +6,9 @@ defmodule Plug.Scope do
     options
   end
 
+  @spec invalid_scope(Plug.Conn.t()) :: Plug.Conn.t()
   defp invalid_scope(conn) do
     conn
-    |> put_resp_content_type("application/json")
     |> send_resp(403, "{\"error\":\"Insufficient scope\"}")
     |> halt
   end
@@ -16,6 +16,6 @@ defmodule Plug.Scope do
   @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
   def call(%Plug.Conn{request_path: path} = conn, _opts) do
     conn
-    |> halt
+    |> put_resp_content_type("application/json")
   end
 end
