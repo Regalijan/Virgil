292221e850dcbd9910b293bc22e67d6a4853ed15
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-08-22 23:13:58 -0400

    Slightly update auth token check

--- api/lib/api/plugs/auth.ex
+++ api/lib/api/plugs/auth.ex
@@ -7,18 +7,34 @@ defmodule Plug.TokenAuth do
   end
 
   defp authenticate({conn, token}) do
-    token_entry = Mongo.find_one(
-      :mongo,
-      "tokens",
-      %{
-        token_hash:
-          :crypto.hash("sha256", token)
-          |> :base64.encode_to_string
-      }
-    )
-    case token === System.get_env("API_TOKEN", "") do
+    token_entry =
+      Mongo.find_one(
+        :mongo,
+        "tokens",
+        %{
+          token_hash:
+            :crypto.hash("sha256", token)
+            |> :base64.encode_to_string()
+        }
+      )
+
+    case is_nil(token_entry) do
+      true -> send_401(conn)
+      false -> conn
+    end
+  end
+
+  defp check_if_token_nil({conn, token}) do
+    case token == nil do
       false -> send_401(conn)
       true -> conn
+    end
+  end
+
+  defp get_auth_header(conn) do
+    case get_req_header(conn, "authorization") do
+      [token] -> {conn, token}
+      _ -> {conn, nil}
     end
   end
 
@@ -27,13 +43,6 @@ defmodule Plug.TokenAuth do
     |> put_resp_content_type("application/json")
     |> send_resp(401, "{\"error\":\"Unauthorized\"}")
     |> halt
-  end
-
-  defp get_auth_header(conn) do
-    case get_req_header(conn, "authorization") do
-      [token] -> {conn, token}
-      _ -> {conn, nil}
-    end
   end
 
   @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
