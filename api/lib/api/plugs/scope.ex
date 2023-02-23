dd82874ff805de1be6777dec04883ebfc5303235
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2023-02-23 15:39:25 -0500

    Add more of scope plug

--- api/lib/api/plugs/scope.ex
+++ api/lib/api/plugs/scope.ex
@@ -6,6 +6,16 @@ defmodule Plug.Scope do
     options
   end
 
+  @spec check_resource(Plug.Conn.t(), String) :: Plug.Conn.t()
+  defp check_resource(conn, path) do
+    path_segments = String.split(path, "/", trim: true)
+
+    case List.first(path_segments) == "guild" && conn.assigns[:token_data].type != "user" do
+      true -> validate_scopes(conn, path)
+      false -> conn
+    end
+  end
+
   @spec invalid_scope(Plug.Conn.t()) :: Plug.Conn.t()
   defp invalid_scope(conn) do
     conn
@@ -13,9 +23,23 @@ defmodule Plug.Scope do
     |> halt
   end
 
+  @spec validate_scopes(Plug.Conn.t(), String) :: Plug.Conn.t()
+  defp validate_scopes(conn, path) do
+    path_segments = String.split(path, "/", trim: true)
+    scopes = conn.assigns[:token_data].scopes
+    resource = Enum.at(path_segments, 2)
+    guild = Enum.at(path_segments, 1)
+
+    case Enum.member?(scopes, resource) && conn.assigns[:token_data].guild == guild do
+      false -> invalid_scope(conn)
+      true -> conn
+    end
+  end
+
   @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
   def call(%Plug.Conn{request_path: path} = conn, _opts) do
     conn
     |> put_resp_content_type("application/json")
+    |> check_resource(path)
   end
 end
