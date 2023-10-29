b76c70092f3fa52bf920a0ced938e5c3080babab
Regalijan <72576136+Regalijan@users.noreply.github.com>
2023-10-29 13:48:11 -0400

    Format with prettier

--- commands/selfban.ts
+++ commands/selfban.ts
@@ -13,44 +13,42 @@ export = {
       return;
     }
 
-	const chance = Math.random() <= 1 / 10000;
-	if (chance) {
-		const target = await i.guild?.members.fetch(
-		  i.user
-		);
-		if (
-		  !target?.bannable ||
-		  !i.guild?.members.me ||
-		  target.roles.highest.comparePositionTo(i.guild.members.me.roles.highest) >= 0
-		) {
-		  await i.reply("This user cannot be banned.");
-		  return;
-		}
-		
-		let aheadToUnban = Math.round(Math.random()*10) * 1440 * 60000;
-		await target
-		  .send({
-			content: `You have been banned from ${
-			  i.guild?.name
-			} for the following reason:\n\nSelf-ban`,
-		  })
-		  .catch((e) => console.error(e));
-		await i.reply({
-			content: `${i.user} banned himself successfully.`,
-		})
-		await target.ban({
-		  reason: "Natural Selection",
-		});
-		aheadToUnban += Date.now();
-		await mongo
-		  .db("bot")
-		  .collection("bans")
-		  .insertOne({ server: i.guildId, unban: aheadToUnban, user: target.id });
-	} else {
-		await i.reply({
-			content: "Try again.",
-			ephemeral: true
-		})
-	}
+    const chance = Math.random() <= 1 / 10000;
+    if (chance) {
+      const target = await i.guild?.members.fetch(i.user);
+      if (
+        !target?.bannable ||
+        !i.guild?.members.me ||
+        target.roles.highest.comparePositionTo(
+          i.guild.members.me.roles.highest,
+        ) >= 0
+      ) {
+        await i.reply("This user cannot be banned.");
+        return;
+      }
+
+      let aheadToUnban = Math.round(Math.random() * 10) * 1440 * 60000;
+      await target
+        .send({
+          content: `You have been banned from ${i.guild?.name} for the following reason:\n\nSelf-ban`,
+        })
+        .catch((e) => console.error(e));
+      await i.reply({
+        content: `${i.user} banned himself successfully.`,
+      });
+      await target.ban({
+        reason: "Natural Selection",
+      });
+      aheadToUnban += Date.now();
+      await mongo
+        .db("bot")
+        .collection("bans")
+        .insertOne({ server: i.guildId, unban: aheadToUnban, user: target.id });
+    } else {
+      await i.reply({
+        content: "Try again.",
+        ephemeral: true,
+      });
+    }
   },
 };
