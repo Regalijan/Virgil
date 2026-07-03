83746e472e86b4b95f546b02b6bc5d5ac42a0fa7
Regalijan <72576136+Regalijan@users.noreply.github.com>
2026-07-03 04:16:12 -0400

    Add agenda for job scheduling

---
+++ agenda.ts
@@ -0,0 +1,14 @@
+import { Agenda } from "agenda";
+import { MongoBackend } from "@agendajs/mongo-backend";
+import { RedisBackend } from "@agendajs/redis-backend";
+import redis from "./redis";
+
+const redisBackend = new RedisBackend({ redis });
+const agenda = new Agenda({
+  backend: new MongoBackend({
+    address: `${process.env.MONGOURL ?? "mongodb://mongo:27017"}/agenda`,
+  }),
+  notificationChannel: redisBackend.notificationChannel,
+});
+
+export default agenda;
