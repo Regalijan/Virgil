d1db1105d375c63d067fda2eb891b8d45a177809
Regalijan <72576136+Regalijan@users.noreply.github.com>
2026-07-04 01:55:43 -0400

    Nice of the docs to not mention this

--- agenda.ts
+++ agenda.ts
@@ -4,6 +4,11 @@ import { RedisBackend } from "@agendajs/redis-backend";
 import redis from "./redis";
 
 const redisBackend = new RedisBackend({ redis });
+
+(async function () {
+  await redisBackend.connect();
+})();
+
 const agenda = new Agenda({
   backend: new MongoBackend({
     address: `${process.env.MONGOURL ?? "mongodb://mongo:27017"}/agenda`,
