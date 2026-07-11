d9bbdb2a341bd3f720408df13ab1fd5b1a4527cb
Regalijan <72576136+Regalijan@users.noreply.github.com>
2026-07-11 02:02:07 -0400

    Actually apply sticky roles on join

--- events/guildMemberUpdate.ts
+++ events/guildMemberUpdate.ts
@@ -2,6 +2,7 @@ import { EmbedBuilder, GuildMember, PartialGuildMember } from "discord.js";
 import db from "../mongo";
 import Logger from "../logger";
 import SendLog from "../send_log";
+import { type ObjectId } from "mongodb";
 
 const mongo = db.db("bot");
 
@@ -9,32 +10,89 @@ module.exports = async function (
   oldMember: GuildMember | PartialGuildMember,
   newMember: GuildMember | PartialGuildMember,
 ) {
+  const stickyRoles = await mongo
+    .collection("sticky_roles")
+    .find({ guild: newMember.guild.id })
+    .toArray();
+
+  if (
+    oldMember.roles.cache.size !== newMember.roles.cache.size &&
+    stickyRoles.length
+  ) {
+    const oldRoles = oldMember.guild.roles.cache.keys().toArray();
+    const newRoles = newMember.guild.roles.cache.keys().toArray();
+    const wasRoleApplied = newRoles.length > oldRoles.length;
+    const appliedStickyRolesCol = mongo.collection("applied_sticky_roles");
+
+    if (wasRoleApplied) {
+      const addedStickyRoles = newRoles.filter(
+        (role) =>
+          !oldRoles.includes(role) &&
+          stickyRoles.find((sr) => sr.role === role),
+      );
+
+      const additions: { guild: string; role: string; user: string }[] = [];
+
+      addedStickyRoles.forEach((role) => {
+        additions.push({
+          guild: newMember.guild.id,
+          role,
+          user: newMember.user.id,
+        });
+      });
+
+      await appliedStickyRolesCol.insertMany(additions);
+    } else {
+      const removedStickyRoles = oldRoles.filter(
+        (role) =>
+          !newRoles.includes(role) &&
+          stickyRoles.find((sr) => sr.role === role),
+      );
+
+      const removals: { _id: ObjectId }[] = stickyRoles
+        .filter((sr) => removedStickyRoles.includes(sr.role))
+        .map((s) => {
+          return { _id: s._id };
+        });
+
+      await appliedStickyRolesCol.deleteMany(removals);
+    }
+  }
+
   const settings = await mongo
     .collection("settings")
     .findOne({ guild: newMember.guild.id })
     .catch(Logger);
   if (!settings) return;
+
   const embed = new EmbedBuilder();
   embed.setAuthor({
     name: newMember.user.username,
     iconURL: newMember.user.displayAvatarURL(),
   });
+
   if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
     if (!settings.roleLogChannelWebhook) return;
+
     embed.setTitle("Roles Updated");
+
     let oldrolesstring = "";
     oldMember.roles.cache.forEach((r) => {
       oldrolesstring += ` <@&${r.id}>`;
     });
+
     embed.addFields({ name: "Old Roles", value: oldrolesstring });
+
     if (oldMember.roles.cache.size > newMember.roles.cache.size) {
       let rolesremoved = "";
+
       oldMember.roles.cache.each((r) => {
         if (!newMember.roles.cache.has(r.id)) rolesremoved += ` <@&${r.id}>`;
       });
       embed.addFields({ name: "Roles Removed", value: rolesremoved });
     } else {
       let rolesadded = "";
+
       newMember.roles.cache.each((r) => {
         if (!oldMember.roles.cache.has(r.id)) rolesadded += ` <@&${r.id}>`;
       });
@@ -48,6 +106,7 @@ module.exports = async function (
     );
   } else if (oldMember.nickname !== newMember.nickname) {
     if (!settings.nicknameLogChannelWebhook) return;
+
     embed.setTitle("Nickname Updated");
     embed.setDescription(
       `\`${oldMember.nickname ?? "None"}\` -> \`${
