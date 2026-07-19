7a1de548609d3322b31c3990a358da75b5cd7fab
Regalijan <72576136+Regalijan@users.noreply.github.com>
2026-07-19 03:20:48 -0400

    Fix sentry release monitor

--- sentry.ts
+++ sentry.ts
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 
 Sentry.init({
   dsn: process.env.DSN,
-  release: execSync("git rev-parse HEAD", { cwd: __dirname }).toString(),
+  release: execSync("git rev-parse HEAD", { cwd: process.cwd() }).toString(),
 });
 
 export default Sentry;
