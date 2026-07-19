faac2195c7760abde95154258cfb8f82672ea705
Regalijan <72576136+Regalijan@users.noreply.github.com>
2026-07-19 00:19:49 -0400

    Remove redundant env var injection

--- mongo.ts
+++ mongo.ts
@@ -1,7 +1,4 @@
-import { config as dotenv } from "dotenv";
 import { MongoClient } from "mongodb";
-
-dotenv();
 
 const client = new MongoClient(process.env.MONGOURL ?? "mongodb://mongo:27017");
 export default client;
