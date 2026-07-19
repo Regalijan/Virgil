ce8e0336b9958eb1fefd5758d63cd3b99cd42df6
Regalijan <72576136+Regalijan@users.noreply.github.com>
2026-07-19 04:04:34 -0400

    oops

--- commands/banmessage.ts
+++ commands/banmessage.ts
@@ -5,15 +5,6 @@ const messagesStore = mongo.db("bot").collection("ban_messages");
 export const name = "banmessage";
 
 export async function exec(i: ChatInputCommandInteraction): Promise<void> {
-  const settings = await messagesStore.findOne({ guild: i.guildId });
-  if (!settings) {
-    await i.reply({
-      content:
-        "Server settings are not initialized! Please run the initialize command.",
-      flags: [MessageFlagsBitField.Flags.Ephemeral],
-    });
-    return;
-  }
   switch (i.options.getSubcommand(true)) {
     case "set":
       await messagesStore.updateOne(
